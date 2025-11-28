baf663ee403c2da5e899c359b72cc0653591d748
Anne-Siri Trønnes <ntr@ssb.no>
2025-11-28 08:55:55 +0100

    move

--- src/main.ts
+++ src/main.ts
@@ -11,17 +11,13 @@ const expressInstance = express()
 expressInstance.use(helmet())
 expressInstance.use(promBundleMetrics)
 expressInstance.use(controllerRouter)
+// TODO: Remove when initial testing is done
 expressInstance.get('/', (_: Request, res: Response) => res.send('STATREG-API-V1'))
+expressInstance.get('/secret', (_, res) => {
+  res.send('Very secret message!')
+})
 startServer(expressInstance)
 
 const file = fs.readFileSync('./openapi/openapi.yaml', 'utf8')
 const swaggerDocument = YAML.parse(file)
 expressInstance.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-// TODO: Remove when login is working and other endpoints implemented
-expressInstance.get('/secret', (_, res) => {
-  res.send('Very secret message!')
-})
-expressInstance.get('/', (_, res) => {
-  res.send('Hello World!')
-})
