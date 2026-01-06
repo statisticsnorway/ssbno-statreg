ff2a4e6fa0b0bef2c8238c89d78651ad22fcf1c8
Carl-OW <cob@ssb.no>
2026-01-06 13:27:40 +0100

    comments removal

--- src/main.ts
+++ src/main.ts
@@ -21,21 +21,17 @@ const app = express()
 app.use(helmet())
 app.use(promBundleMetrics)
 
-// Public: Swagger
 const file = fs.readFileSync('./openapi/openapi.yaml', 'utf8')
 const swaggerDocument = YAML.parse(file)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-// Public: root
 app.get('/', (_: Request, res: Response) => res.send('STATREG-API-V1'))
 
-// Auth middleware used per route (dev mode bypasses it)
 const requireAuth: RequestHandler = DEVELOPMENT_MODE ? (_req, _res, next) => next() : createAuthMiddleware()
 
 // Controllers decide per endpoint if it is public or protected
 app.use(createControllerRouter(requireAuth))
 
-// Protected: show current auth context
 app.get('/auth/me', requireAuth, (req, res) => {
   res.json({
     token: req.auth?.token,
@@ -45,7 +41,6 @@ app.get('/auth/me', requireAuth, (req, res) => {
   })
 })
 
-// Protected + aud-based access (temporary)
 app.get('/secret', requireAuth, requireAudience('oauth2-proxy-ssbno-statreg-api'), (_req, res) => {
   res.send('Very secret message!')
 })
