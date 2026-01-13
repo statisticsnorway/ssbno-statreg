6a4a94f7408b4cfc59910af6de42a55125dbfb4f
Anne-Siri Trønnes <ntr@ssb.no>
2026-01-13 13:54:07 +0100

    build: small change in p12 file generation

--- run.sh
+++ run.sh
@@ -2,11 +2,18 @@ set -e
 
 # Generate p12-file for ssl, see prisma docs: https://www.prisma.io/docs/orm/overview/databases/postgresql#configuring-an-ssl-connection
 # example: openssl pkcs12 -export -out client-identity.p12 -inkey client-key.pem -in client-cert.pem
-openssl pkcs12 -password pass:$SSBNO_STATREG_API_SSL_PASSWORD -export -out /tmp/client-identity.p12 -inkey $NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLKEY -in $NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLCERT
+
+openssl pkcs12 \
+  -export \
+  -out /tmp/client-identity.p12 \
+  -inkey "$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLKEY" \
+  -in "$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLCERT" \
+  -passout pass:"$SSBNO_STATREG_API_SSL_PASSWORD"
+
 
 # Building connection string from scratch
 # example: postgresql://USER:PASSWORD@HOST:PORT/DATABASE?sslidentity=client-identity.p12&sslpassword=mypassword&sslcert=rootca.cert
-export STATREG_DB_URL_CONNECTION_STRING="postgresql://$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_USERNAME:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PASSWORD@$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_HOST:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PORT/$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_DATABASE?sslidentity=/tmp/client-identity.p12&sslpassword=$SSBNO_STATREG_API_SSL_PASSWORD&sslcert=$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLROOTCERT"
+export STATREG_DB_URL_CONNECTION_STRING="postgresql://$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_USERNAME:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PASSWORD@$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_HOST:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PORT/$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_DATABASE?sslidentity=/tmp/client-identity.p12&sslpassword=$SSBNO_STATREG_API_SSL_PASSWORD&sslcert=$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLROOTCERT&sslmode=verify-ca"
 
 npm run db:deploy
 npm run seed
