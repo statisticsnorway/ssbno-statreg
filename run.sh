28693e871cf46807ad4b41be0f50580ead0e373d
Anne-Siri Trønnes <ntr@ssb.no>
2026-01-13 13:40:36 +0100

    build: try with db:deploy and seeding

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ openssl pkcs12 -password pass:$SSBNO_STATREG_API_SSL_PASSWORD -export -out /tmp/
 # example: postgresql://USER:PASSWORD@HOST:PORT/DATABASE?sslidentity=client-identity.p12&sslpassword=mypassword&sslcert=rootca.cert
 export STATREG_DB_URL_CONNECTION_STRING="postgresql://$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_USERNAME:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PASSWORD@$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_HOST:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PORT/$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_DATABASE?sslidentity=/tmp/client-identity.p12&sslpassword=$SSBNO_STATREG_API_SSL_PASSWORD&sslcert=$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLROOTCERT"
 
-# npm run db:deploy
+npm run db:deploy
+npm run seed
 # npx prisma migrate deploy 
 # npx prisma generate
 exec npm run start
