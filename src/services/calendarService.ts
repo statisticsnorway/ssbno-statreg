acfdc95a2851c69151f0065cb4d943b051f33ebc
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-20 13:14:01 +0100

    increment id

--- src/services/calendarService.ts
+++ src/services/calendarService.ts
@@ -2,9 +2,12 @@ import { Calender_date } from '@/generated/prisma/client'
 import { prisma } from '@/lib/prisma'
 
 export async function createBlockedReleaseDay(date: string, blocked_comment: string): Promise<Calender_date> {
+  const highestId = (await prisma.calender_date.findFirst({ orderBy: { id: 'desc' } }))?.id
+  const incrementId = Number(highestId) + 1
   return prisma.calender_date.create({
     data: {
-      id: 5,
+      //TODO: Id should autoincrement
+      id: incrementId,
       version: 0,
       comment: blocked_comment,
       day: new Date(date),
