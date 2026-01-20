d8cc46fbe8c05bdfe34e5251bf04707c56362c88
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-20 12:47:15 +0100

    readd

--- src/services/calendarService.ts
+++ src/services/calendarService.ts
@@ -4,6 +4,7 @@ import { prisma } from '@/lib/prisma'
 export async function createBlockedReleaseDay(date: string, blocked_comment: string): Promise<Calender_date>  {
   return prisma.calender_date.create({
     data: {
+      id: 2,
       version: 0,
       comment: blocked_comment,
       day: new Date(date),
