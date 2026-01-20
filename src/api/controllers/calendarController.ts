583be40f3c5195f3ef8b3da50172bbb4f65e4aac
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-20 13:19:04 +0100

    add auth

--- src/api/controllers/calendarController.ts
+++ src/api/controllers/calendarController.ts
@@ -1,9 +1,9 @@
 import { createBlockedReleaseDay } from '@/services/calendarService'
 import { Router } from 'express'
-import { skipAuth } from 'plugins/authMiddleware'
+import { requireAudience } from 'plugins/authMiddleware'
 
 export default function calendarController(router: Router) {
-  router.post('/calendar/blocked-release-days/:date', skipAuth, async (req, res) => {
+  router.post('/calendar/blocked-release-days/:date', requireAudience('oauth2-proxy-ssbno-statreg-api'), async (req, res) => {
     const { blocked_comment } = req.body
     const date = req.params.date
     const result = await createBlockedReleaseDay(date, blocked_comment)
