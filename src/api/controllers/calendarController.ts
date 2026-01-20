23793411899b93b7fd8517775bd37108918e5321
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-20 14:16:29 +0100

    linting

--- src/api/controllers/calendarController.ts
+++ src/api/controllers/calendarController.ts
@@ -3,10 +3,14 @@ import { Router } from 'express'
 import { requireAudience } from 'plugins/authMiddleware'
 
 export default function calendarController(router: Router) {
-  router.post('/calendar/blocked-release-days/:date', requireAudience('oauth2-proxy-ssbno-statreg-api'), async (req, res) => {
-    const { blocked_comment } = req.body
-    const date = req.params.date
-    const result = await createBlockedReleaseDay(date, blocked_comment)
-    res.json(result)
-  })
+  router.post(
+    '/calendar/blocked-release-days/:date',
+    requireAudience('oauth2-proxy-ssbno-statreg-api'),
+    async (req, res) => {
+      const { blocked_comment } = req.body
+      const date = req.params.date
+      const result = await createBlockedReleaseDay(date, blocked_comment)
+      res.json(result)
+    }
+  )
 }
