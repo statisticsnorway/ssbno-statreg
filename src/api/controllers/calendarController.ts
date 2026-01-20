5cedf00a86be176e953100ed27edf3a8f03308ab
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-20 13:14:31 +0100

    remove console.log

--- src/api/controllers/calendarController.ts
+++ src/api/controllers/calendarController.ts
@@ -4,7 +4,6 @@ import { skipAuth } from 'plugins/authMiddleware'
 
 export default function calendarController(router: Router) {
   router.post('/calendar/blocked-release-days/:date', skipAuth, async (req, res) => {
-    console.log('Post blocked', req.body)
     const { blocked_comment } = req.body
     const date = req.params.date
     const result = await createBlockedReleaseDay(date, blocked_comment)
