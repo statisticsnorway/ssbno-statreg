833acc72d4408919e461a90fd2147c665325e4d1
Glenruben Larsen <gle@ssb.no>
2026-01-27 13:16:07 +0100

    trying to specify certs. wow.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,11 +2,15 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client.js'
 import process from 'node:process'
 import 'dotenv/config'
+import * as fs from 'node:fs'
 
 const adapter = new PrismaPg({
   connectionString: process.env.STATREG_DB_URL_CONNECTION_STRING!,
   ssl: {
     rejectUnauthorized: false,
+    ca: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/root-cert.pem').toString(),
+    key: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/key.pem').toString(),
+    cert: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/cert.pem').toString(),
   },
 })
 
