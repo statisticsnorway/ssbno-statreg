de8a72e7cc4e74fabc6337c9aae3741ecab2e378
Glenruben Larsen <gle@ssb.no>
2026-01-28 14:29:27 +0100

    try new seed config, after upgrading prisma. Fingers crossed!

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,17 +4,9 @@ import process from 'node:process'
 
 import { PrismaClient } from '../src/generated/prisma/client.js'
 import { Env } from '../prisma.config.js'
-import * as fs from 'node:fs'
-// import { connect } from 'node:http2'
 
 const adapter = new PrismaPg({
   connectionString: env<Env>('STATREG_DB_URL_CONNECTION_STRING'),
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/root-cert.pem', 'utf8'),
-    key: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/key.pem', 'utf8'),
-    cert: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/cert.pem', 'utf-8'),
-  },
 })
 
 const prisma = new PrismaClient({ adapter })
--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ chmod 644 /tmp/client-identity.p12
 # example: postgresql://USER:PASSWORD@HOST:PORT/DATABASE?sslidentity=client-identity.p12&sslpassword=mypassword&sslcert=rootca.cert
 export STATREG_DB_URL_CONNECTION_STRING="postgresql://$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_USERNAME:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PASSWORD@$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_HOST:$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_PORT/$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_DATABASE?sslidentity=/tmp/client-identity.p12&sslpassword=$SSBNO_STATREG_API_SSL_PASSWORD&sslcert=$NAIS_DATABASE_SSBNO_STATREG_API_STATREG_DB_SSLROOTCERT&sslmode=require"
 
-# npm run db:deploy
-# npm run seed
+npm run db:deploy
+npm run seed
 
 exec npm run start
