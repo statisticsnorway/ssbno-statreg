59bf02f094da322a7bd5b8d1192a62aad8c29717
johnnadeluy <johnnadel.salita@gmail.com>
2026-01-29 08:09:10 +0100

    fix: Remove ssl object from /lib/primsa so build doesn't fail

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,16 +2,9 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client.js'
 import process from 'node:process'
 import 'dotenv/config'
-import * as fs from 'node:fs'
 
 const adapter = new PrismaPg({
   connectionString: process.env.STATREG_DB_URL_CONNECTION_STRING!,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/root-cert.pem', 'utf8'),
-    key: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/key.pem', 'utf8'),
-    cert: fs.readFileSync('/var/run/secrets/nais.io/sqlcertificate/cert.pem', 'utf-8'),
-  },
 })
 
 const prisma = new PrismaClient({ adapter })
