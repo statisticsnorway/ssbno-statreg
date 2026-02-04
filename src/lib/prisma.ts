0f9d560bff07574ca28dc29dcc907b740c3b4d40
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-04 08:00:27 +0100

    fix: Add conditional to SSL settings, and remove read-only-file-system set to false in application manifest

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,10 +5,13 @@ import 'dotenv/config'
 
 const adapter = new PrismaPg({
   connectionString: process.env.PGURL!,
-  // TODO: Disable ssl in local development
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // SSL settings need to be disabled for local development, otherwise prisma db seed will fail,
+  // and the NAIS_CLUSTER_NAME is a default variable that is either test or prod
+  ssl: process.env.NAIS_CLUSER_NAME
+    ? {
+        rejectUnauthorized: false,
+      }
+    : undefined,
 })
 const prisma = new PrismaClient({ adapter })
 
