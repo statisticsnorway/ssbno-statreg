ffaa8a0495b62ab930559d880295e5e71b1a0e1c
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-09 14:31:10 +0100

    Use openapi-typescript instead and manually define types in index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import type { components } from './api-types'
+
+export type Statistics = components['schemas']['Statistic_details']
+export type Statistic = components['schemas']['Statistic_listing']
