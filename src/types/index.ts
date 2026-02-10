e223d2129dc0adab99ffe6cbc0f253b6dbd863fa
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-10 08:24:44 +0100

    refactor: Use pascal case for type names

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,11 @@
 import type { components } from './api-types'
 
-export type Blocked_release_date = components['schemas']['Blocked_release_date']
-export type Calender_date = components['schemas']['Calender_date']
+export type BlockedReleaseDate = components['schemas']['Blocked_release_date']
+export type CalenderDate = components['schemas']['Calender_date']
 export type Contact = components['schemas']['Contact']
 export type Frequency = components['schemas']['Frequency']
 export type Release = components['schemas']['Release']
-export type Statistic_details = components['schemas']['Statistic_details']
-export type Statistic_listing = components['schemas']['Statistic_listing']
+export type StatisticDetails = components['schemas']['Statistic_details']
+export type StatisticListing = components['schemas']['Statistic_listing']
 export type Translations = components['schemas']['Translations']
 export type Variant = components['schemas']['Variant']
