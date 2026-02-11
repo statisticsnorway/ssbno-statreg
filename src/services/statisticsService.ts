36cc99386ecfabf55ee6f5a75d825e43f6fc1d2c
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-11 13:23:18 +0100

    Filter statistics query to prisma

--- src/services/statisticsService.ts
+++ src/services/statisticsService.ts
@@ -2,7 +2,22 @@ import { Statistic } from '@/generated/prisma/client'
 import { prisma } from '../lib/prisma'
 
 export async function getAllStatistics(): Promise<Statistic[]> {
-  const statistics = await prisma.statistic.findMany({ include: { shortname: true, division: true } })
+  const statistics = await prisma.statistic.findMany({
+    omit: {
+      id: true,
+      dir_appoval_status: true,
+      search_phrases: true,
+      search_phrases_en: true,
+      priority: true,
+      relation_id: true,
+      last_updated: true,
+      date_created: true,
+      legacy_topic_codes: true,
+    },
+    include: { shortname: { select: { name: true } }, division: { select: { name: true, name_en: true } } },
+  })
+
+  console.log(statistics)
 
   return statistics.map((statistic) => {
     const main_language = statistic.language // Either nb or nn
