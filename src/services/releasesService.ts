205bb15d7a63b467cacbe6439bbeb58e1b4a9f2e
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-11 14:40:48 +0100

    refactor: Fix types for releases service

--- src/services/releasesService.ts
+++ src/services/releasesService.ts
@@ -1,22 +1,22 @@
-import { Release } from '@/generated/prisma/client'
+import { Release } from '../types/index'
 import { prisma } from '../lib/prisma'
 
 export async function getAllReleases(): Promise<Release[]> {
   const releases = await prisma.release.findMany()
 
   return releases.map((release) => ({
-    id: release.id,
-    version: release.version,
-    published_at: release.publish_time,
+    id: release.id.toString(),
+    version: release.version.toString(),
+    published_at: release.publish_time.toISOString(),
     has_versions: release.has_versions,
-    updated_at: release.last_updated,
+    updated_at: release.last_updated.toISOString(),
     comment: release.comment,
     desk_appoval_status: release.desk_appoval_status,
-    variant_id: release.variant_id,
-    period_to: release.period_to,
-    period_from: release.period_from,
+    variant_id: release.variant_id.toString(),
+    period_to: release.period_to.toISOString(),
+    period_from: release.period_from.toISOString(),
     cancelled: release.cancelled,
-    created_at: release.date_created,
+    created_at: release.date_created.toISOString(),
     release_date_precision: release.release_date_precision,
     import_flag: release.import_flag,
   }))
