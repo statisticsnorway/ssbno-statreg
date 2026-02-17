5246f3323ecd144b4135a20809ed5b75fdb159ff
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-17 09:09:41 +0100

    test: Update test with new url for klassService; add more tests in a separate PR

--- src/services/releasesService.ts
+++ src/services/releasesService.ts
@@ -7,7 +7,7 @@ export async function getAllReleases({ start = 0, count = 10 }): Promise<Release
     take: count,
   })
 
-  // TODO: MIM-2475. Go over the openapi spec in a separate PR and fix mapping accordingly
+  // TODO: Go over the openapi spec in a new PR for MIM-2475 and fix mapping accordingly
   return releases.map((release) => ({
     id: release.id.toString(),
     version: release.version.toString(),
