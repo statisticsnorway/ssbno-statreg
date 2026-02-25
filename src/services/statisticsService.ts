a0653a6585430592efe6fb11e478d93590a91c01
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-25 14:49:27 +0100

    fix: Update statistic details values

--- src/services/statisticsService.ts
+++ src/services/statisticsService.ts
@@ -39,12 +39,47 @@ export async function getAllStatistics(
   })
 }
 
-export async function getStatisticByShortname(shortname: string): Promise<StatisticDetails> {
+function parseStatisticVariants(variants) {
+  if (!variants?.length) return
+
+  return variants.map((variant) => ({
+    version: variant.version,
+    updated_at: dateToISOString(variant.last_updated),
+    level_of_detail: [
+      {
+        language_code: 'nb',
+        text: variant.level_of_detail,
+      },
+      {
+        language_code: 'en',
+        text: variant.level_of_detail_en,
+      },
+    ],
+    created_at: dateToISOString(variant.date_created),
+    cancelled: variant.cancelled,
+    freqency: {
+      name: [...getLocalizedName('nb', variant.frequency.name), ...getLocalizedName('en', variant.frequency.name_en)],
+    },
+    revision: variant.revision,
+  }))
+}
+
+export async function getStatisticByShortname(shortname: string, prisma: StatisticPrisma): Promise<StatisticDetails> {
   const statistic = await prisma.statistic.findFirst({
     where: { shortname: { name: shortname } },
     include: {
       shortname: { select: { name: true } },
       responsiblePersons: { select: { email: true } },
+      statistic: { select: { language: true, name: true, name_en: true, shortname: true } },
+      statistic_region_levels: {
+        select: { region_level: { select: { name: true } } },
+      },
+      variants: {
+        omit: { id: true, statistic_id: true, freq_id: true },
+        include: {
+          frequency: { select: { name: true, name_en: true } },
+        },
+      },
     },
   })
 
@@ -54,6 +89,7 @@ export async function getStatisticByShortname(shortname: string): Promise<Statis
   const lang_en = 'en'
 
   const division_code = statistic.division_code
+  const related_statistic = statistic.statistic
 
   return {
     version: statistic.version,
@@ -69,34 +105,24 @@ export async function getStatisticByShortname(shortname: string): Promise<Statis
     },
     first_released_at: dateToISOString(statistic.first_release),
     yearly_reporting: statistic.yearly_reporting,
-    status: [
-      {
-        language_code: main_language,
-        text: statistic.status,
-      },
-    ],
+    status: {
+      code: statistic.status,
+    },
     previous_topic_codes: statistic.legacy_topic_codes,
+    // TODO: Add to seed
     relation: {
-      id: 'string',
+      shortname: related_statistic?.shortname?.name,
       name: [
-        {
-          language_code: 'string',
-          text: 'string',
-        },
+        ...getLocalizedName(related_statistic?.language, related_statistic?.name),
+        ...getLocalizedName(lang_en, related_statistic?.name_en),
       ],
     },
     name: [...getLocalizedName(main_language, statistic.name), ...getLocalizedName(lang_en, statistic.name_en)],
     updated_at: dateToISOString(statistic.last_updated),
     comment: statistic.comment,
     created_at: dateToISOString(statistic.date_created),
+    variants: parseStatisticVariants(statistic.variants),
     contacts: statistic.responsiblePersons,
-    statistic_region_levels: [
-      [
-        {
-          language_code: 'string',
-          text: 'string',
-        },
-      ],
-    ],
+    statistic_region_levels: [], // TODO: Add to seed
   }
 }
