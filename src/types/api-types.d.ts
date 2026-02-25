ff7b5033a30f04a12a4b49f1c8f530620da0fc70
johnnadeluy <johnnadel.salita@gmail.com>
2026-02-25 13:20:28 +0100

    fix: Update openapi spec for statistic detailed

--- src/types/api-types.d.ts
+++ src/types/api-types.d.ts
@@ -699,16 +699,18 @@ export interface components {
       approval_status?: string | null
       main_language?: string
       division?: {
-        code?: unknown
+        code?: string
         readonly name?: components['schemas']['Translations']
       }
       /** Format: date */
       first_released_at?: string | null
       yearly_reporting?: boolean
-      status?: components['schemas']['Translations']
+      status?: {
+        code?: string
+      }
       previous_topic_codes?: string | null
       relation?: {
-        id?: unknown
+        shortname?: string
         readonly name?: components['schemas']['Translations']
       }
       name?: components['schemas']['Translations']
