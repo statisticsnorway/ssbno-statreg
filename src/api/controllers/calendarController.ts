3ca8677dbd04d8cadbcd09a1f734efd041021f00
Glenruben Larsen <gle@ssb.no>
2026-03-16 12:54:03 +0100

    WIP adds put and delete for calendarDate, to test auditlog

--- src/api/controllers/calendarController.ts
+++ src/api/controllers/calendarController.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { handleErrors } from '@/lib/prismaErrors'
-import { createBlockedReleaseDay } from '@/services/calendarService'
+import { createBlockedReleaseDay, deleteBlockedReleaseDay, updateBlockedReleaseDay } from '@/services/calendarService'
 import { Router } from 'express'
 import { requireUserGroupAuthorization } from 'plugins/authMiddleware'
 
@@ -11,6 +11,32 @@ export default function calendarController(router: Router) {
     async (req, res) => {
       try {
         const result = await createBlockedReleaseDay(prisma, req.params.date, req.body)
+        res.json(result)
+      } catch (error) {
+        handleErrors(error, res)
+      }
+    }
+  )
+
+  router.put(
+    '/calendar/blocked-release-days/:date',
+    requireUserGroupAuthorization('ssbno-developers'),
+    async (req, res) => {
+      try {
+        const result = await updateBlockedReleaseDay(prisma, req.params.date, req.body)
+        res.json(result)
+      } catch (error) {
+        handleErrors(error, res)
+      }
+    }
+  )
+
+  router.delete(
+    '/calendar/blocked-release-days/:date',
+    requireUserGroupAuthorization('ssbno-developers'),
+    async (req, res) => {
+      try {
+        const result = await deleteBlockedReleaseDay(prisma, req.params.date)
         res.json(result)
       } catch (error) {
         handleErrors(error, res)
--- src/services/calendarService.ts
+++ src/services/calendarService.ts
@@ -44,3 +44,78 @@ export async function createBlockedReleaseDay(
     date: dateToISOString(blockedDay.day),
   }))
 }
+
+export async function updateBlockedReleaseDay(
+  prisma: CalendarDatePrisma,
+  dateString?: string | string[],
+  body?: { blocked_comment?: string }
+): Promise<BlockedReleaseDate[]> {
+  if (!body?.blocked_comment) return Promise.reject({ statregError: 'Invalid body' })
+  const blocked_comment = sanitize(body!.blocked_comment!)
+
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+  if (!dateString || Array.isArray(dateString) || !dateRegex.test(dateString)) {
+    return Promise.reject({ statregError: 'Invalid date format in query parameter' })
+  }
+
+  const date = new Date(dateString) //TODO: Confirm correct date format. See JIRA issue MIM-2546
+  if (date.toString() === 'Invalid Date') {
+    return Promise.reject({ statregError: 'Invalid date format in query parameter' })
+  }
+
+  await prisma.calender_date.update({
+    where: { day: date },
+    data: {
+      version: 0,
+      comment: blocked_comment,
+      day: date,
+    },
+  })
+
+  const blockedDays = await prisma.calender_date.findMany({
+    where: {
+      day: {
+        gt: new Date(),
+      },
+    },
+    select: { comment: true, day: true },
+  })
+
+  return blockedDays.map((blockedDay) => ({
+    blocked_comment: blockedDay.comment,
+    date: dateToISOString(blockedDay.day),
+  }))
+}
+
+export async function deleteBlockedReleaseDay(
+  prisma: CalendarDatePrisma,
+  dateString?: string | string[]
+): Promise<BlockedReleaseDate[]> {
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+  if (!dateString || Array.isArray(dateString) || !dateRegex.test(dateString)) {
+    return Promise.reject({ statregError: 'Invalid date format in query parameter' })
+  }
+
+  const date = new Date(dateString) //TODO: Confirm correct date format. See JIRA issue MIM-2546
+  if (date.toString() === 'Invalid Date') {
+    return Promise.reject({ statregError: 'Invalid date format in query parameter' })
+  }
+
+  await prisma.calender_date.delete({
+    where: { day: date },
+  })
+
+  const blockedDays = await prisma.calender_date.findMany({
+    where: {
+      day: {
+        gt: new Date(),
+      },
+    },
+    select: { comment: true, day: true },
+  })
+
+  return blockedDays.map((blockedDay) => ({
+    blocked_comment: blockedDay.comment,
+    date: dateToISOString(blockedDay.day),
+  }))
+}
