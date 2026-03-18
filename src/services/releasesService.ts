a98fa7499cefe0b4b16f04df8e68ffea7ee0be83
Anne-Siri <142512638+annesiri@users.noreply.github.com>
2026-03-18 12:29:50 +0100

    Added endpoint GET /releases/{id} (#138)
    * feat: add endpoint for GET releases/:id
    
    * docs: update openApi-spec for release_details
    
    * update variant object
    
    * updated service according to updated openapi spec
    
    * Feat: Added endpoint GET releases/:id
    
    * improved error handling
    
    * fixed test

--- src/services/releasesService.ts
+++ src/services/releasesService.ts
@@ -1,8 +1,9 @@
-import type { ReleaseListing } from '@/types/index'
-import { getLocalizedName, dateToISOString } from '@/lib/utils'
+import type { ReleaseDetails, ReleaseListing } from '@/types/index'
+import { getLocalizedName, dateToISOString, sanitize } from '@/lib/utils'
 import { ExtendedPrismaClient as PrismaClient } from '@/lib/prisma'
 
 type ReleasePrisma = Pick<PrismaClient, 'release'>
+const lang_en = 'en'
 
 export async function getAllReleases({ start = 0, count = 10 }, prisma: ReleasePrisma): Promise<ReleaseListing[]> {
   const releases = await prisma.release.findMany({
@@ -42,7 +43,6 @@ export async function getAllReleases({ start = 0, count = 10 }, prisma: ReleaseP
 
   return releases.map((release) => {
     const { statistic, frequency } = release.variant ?? {}
-    const lang_en = 'en'
 
     return {
       id: release.id,
@@ -62,4 +62,77 @@ export async function getAllReleases({ start = 0, count = 10 }, prisma: ReleaseP
       },
     }
   })
+}
+
+export async function getReleaseById(id: string, prisma: ReleasePrisma): Promise<ReleaseDetails> {
+  const idAsNumber = Number.parseInt(sanitize(id))
+  if (isNaN(idAsNumber)) {
+    return Promise.reject({ statregError: 'Invalid release id' })
+  }
+
+  const release = await prisma.release.findFirst({
+    where: { id: idAsNumber },
+    select: {
+      id: true,
+      version: true,
+      publish_time: true,
+      desk_appoval_status: true,
+      period_to: true,
+      period_from: true,
+      release_date_precision: true,
+      cancelled: true,
+      variant: {
+        select: {
+          id: true,
+          frequency: {
+            select: {
+              name: true,
+              name_en: true,
+            },
+          },
+          revision: true,
+          statistic: {
+            select: {
+              language: true,
+              name: true,
+              name_en: true,
+              shortname: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+
+  if (!release) return Promise.reject({ status: 404, statregError: 'Release id not found' })
+
+  const { statistic, frequency } = release.variant ?? {}
+
+  return {
+    id: release.id,
+    publish_time: dateToISOString(release.publish_time),
+    has_versions: release.version > 1,
+    approval_status: release.desk_appoval_status,
+    variant: {
+      id: release.variant.id,
+      frequency: {
+        name: [...getLocalizedName('nb', frequency.name), ...getLocalizedName(lang_en, frequency.name_en)],
+      },
+      revision: {
+        name: [...getLocalizedName('nb', release.variant.revision)],
+      },
+    },
+    statistic: {
+      shortname: statistic.shortname.name,
+      name: [...getLocalizedName(statistic.language, statistic.name), ...getLocalizedName(lang_en, statistic.name_en)],
+    },
+    period_from: dateToISOString(release.period_from),
+    period_to: dateToISOString(release.period_to),
+    release_date_precision: release.release_date_precision,
+    cancelled: release.cancelled,
+  }
 }
