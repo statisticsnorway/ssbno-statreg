392f351865f23c387dd6c3a1d970b652033fb434
Olav Landsverk <stud-oll@ssb.no>
2026-03-19 12:03:39 +0100

    add updateRelease method
    Signed-off-by: Olav Landsverk <stud-oll@ssb.no>

--- src/services/releasesService.ts
+++ src/services/releasesService.ts
@@ -1,9 +1,44 @@
-import type { ReleaseDetails, ReleaseListing, ReleaseCreate } from '@/types/index'
+import type { ReleaseDetails, ReleaseListing, ReleaseCreate, ReleaseUpdate } from '@/types/index'
 import { getLocalizedName, dateToISOString, sanitize } from '@/lib/utils'
 import { ExtendedPrismaClient as PrismaClient } from '@/lib/prisma'
 
 type ReleasePrisma = Pick<PrismaClient, 'release'>
 const lang_en = 'en'
+
+const SELECT_RELEASE_DETAILS = {
+  id: true,
+  version: true,
+  publish_time: true,
+  desk_appoval_status: true,
+  period_to: true,
+  period_from: true,
+  release_date_precision: true,
+  cancelled: true,
+  variant: {
+    select: {
+      id: true,
+      frequency: {
+        select: {
+          name: true,
+          name_en: true,
+        },
+      },
+      revision: true,
+      statistic: {
+        select: {
+          language: true,
+          name: true,
+          name_en: true,
+          shortname: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
 
 export async function getAllReleases({ start = 0, count = 10 }, prisma: ReleasePrisma): Promise<ReleaseListing[]> {
   const releases = await prisma.release.findMany({
@@ -149,4 +184,28 @@ export async function createRelease(
   if (!body) return Promise.reject({ statregError: 'Invalid body' })
 
   return {}
+}
+
+export async function updateRelease(id: string, input: ReleaseUpdate, prisma: ReleasePrisma): Promise<ReleaseDetails> {
+  const idAsNumber = Number.parseInt(sanitize(id))
+  if (isNaN(idAsNumber)) {
+    return Promise.reject({ statregError: 'Invalid release id' })
+  }
+
+  const release = await prisma.release.update({
+    select: SELECT_RELEASE_DETAILS,
+    where: { id: idAsNumber },
+    data: {
+      publish_time: input.publish_time,
+      period_to: input.period_to,
+      period_from: input.period_from,
+      release_date_precision: input.release_date_precision,
+    },
+  })
+
+  if (!release) return Promise.reject({ status: 404, statregError: 'Release id not found' })
+
+  return {
+    id: release.id,
+  }
 }
