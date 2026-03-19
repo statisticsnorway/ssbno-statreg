9250ea5ae4da39a5257149e8e51bcbce5c217362
johnnadeluy <johnnadel.salita@gmail.com>
2026-03-19 13:11:58 +0100

    refactor: Add return types to util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,11 @@
+import { Translations } from '@/types'
+
 // For usage, remember to destructurize the function's content e.g. { name: [...getLocalizedName(language_code, text)] }
-export function getLocalizedName(language_code = 'nb', text: string | undefined | null) {
+export function getLocalizedName(language_code = 'nb', text: string | undefined | null): Translations {
   return text ? [{ language_code, text }] : []
 }
 
-export function dateToISOString(date: Date | null) {
+export function dateToISOString(date: Date | null): string | undefined {
   return date ? date.toISOString() : undefined
 }
 
@@ -13,7 +15,7 @@ export function sanitize(input: string): string {
   return input.trim().replace(/[^a-zA-Z0-9æøåÆØÅ.,:;!?()/\-\s]/g, '')
 }
 
-export function validateAndParseDate(dateString: string | string[] | undefined, fieldName?: string) {
+export function validateAndParseDate(dateString: string | string[] | undefined, fieldName?: string): Date {
   const dateRegEx = /^\d{4}-\d{2}-\d{2}$/
   if (!dateString || Array.isArray(dateString) || !dateRegEx.test(dateString)) {
     throw {
