eb2c64c97cf45f6b56f60d8e85bc2bcf240da99c
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2026-03-19 07:39:48 +0100

    feat: Update releases controller for POST of new releases [MIM-2574] (#146)
    * feat: Add POST /statistic/:shortname/variants/:id/release to release controller
    
    * docs: Add TODO comment
    
    * refactor: Move prisma param since body is optional for createRelease

--- src/services/releasesService.ts
+++ src/services/releasesService.ts
@@ -1,4 +1,4 @@
-import type { ReleaseDetails, ReleaseListing } from '@/types/index'
+import type { ReleaseDetails, ReleaseListing, ReleaseCreate } from '@/types/index'
 import { getLocalizedName, dateToISOString, sanitize } from '@/lib/utils'
 import { ExtendedPrismaClient as PrismaClient } from '@/lib/prisma'
 
@@ -135,4 +135,18 @@ export async function getReleaseById(id: string, prisma: ReleasePrisma): Promise
     release_date_precision: release.release_date_precision,
     cancelled: release.cancelled,
   }
+}
+
+export async function createRelease(
+  prisma: ReleasePrisma,
+  shortname: string,
+  variantId: string,
+  body?: ReleaseCreate
+): Promise<ReleaseDetails> {
+  // TODO: Implement logic in another PR; log params to prevent lint from failing
+  console.log(`shortname: ${shortname}, (variant) id: ${variantId}, prisma: ${prisma}`)
+
+  if (!body) return Promise.reject({ statregError: 'Invalid body' })
+
+  return {}
 }
