8288054b42c65b42a3ebf34b2487f80c6f46d437
Olav Landsverk <stud-oll@ssb.no>
2026-04-09 09:41:24 +0200

    easter formula
    Signed-off-by: Olav Landsverk <stud-oll@ssb.no>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -74,3 +74,25 @@ export function ensureRequiredFieldsExists<T extends Record<string, any>>(
 export function isNumber(str: string | number | undefined | null) {
   return Number.isInteger(Number(str))
 }
+
+function calculateEasterDay(year: number): Date {
+  const a = year % 19
+  const b = Math.floor(year / 100)
+  const c = year % 100
+  const d = Math.floor(b / 4)
+  const e = b % 4
+  const f = Math.floor((b + 8) / 25)
+  const g = Math.floor((b - f + 1) / 3)
+  const h = (19 * a + b - d - g + 15) % 30
+  const i = Math.floor(c / 4)
+  const k = c % 4
+  const l = (32 + 2 * e + 2 * i - h - k) % 7
+  const m = Math.floor((a + 11 * h + 22 * l) / 451)
+  const n = Math.floor((h + l - 7 * m + 114) / 31)
+  const p = (h + l - 7 * m + 114) % 31
+
+  const month = n - 1
+  const day = p + 1
+
+  return new Date(year, month, day)
+}
