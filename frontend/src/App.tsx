9238098addd8bb4cc45ace311baa8172d0732d6d
Anne-Siri Trønnes <ntr@ssb.no>
2026-04-17 13:32:06 +0200

    Moved code to component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,6 @@
-import { useEffect, useState } from 'react'
-import client from './api'
-import type { components } from '../../shared/api-types'
-
-type Release = components['schemas']['Release_details']
+import ReleaseDetail from './views/releaseDetail'
 
 function App() {
-  const [release, setReleases] = useState<Release>({})
-
-  useEffect(() => {
-    async function fetchRelease() {
-      const { data, error } = await client.GET('/releases/{id}', { params: { path: { id: '1' } } })
-      if (error) {
-        // handle error
-      } else {
-        setReleases(data)
-      }
-    }
-    fetchRelease()
-  }, [])
-
-  return (
-    <div>
-      <h1>Release {release.id}</h1>
-      <ul>
-        <li>From {release.period_from}</li>
-        <li>To {release.period_to}</li>
-      </ul>
-    </div>
-  )
+  return <ReleaseDetail></ReleaseDetail>
 }
-
 export default App
---
+++ frontend/src/views/releaseDetail.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import client from '../api'
+import type { components } from '../../../shared/api-types'
+
+type Release = components['schemas']['Release_details']
+
+function ReleaseDetail() {
+  const [release, setReleases] = useState<Release>({})
+
+  useEffect(() => {
+    async function fetchRelease() {
+      const { data, error } = await client.GET('/releases/{id}', { params: { path: { id: '1' } } })
+      if (error) {
+        console.log(error)
+        alert(error)
+      } else {
+        setReleases(data)
+      }
+    }
+    fetchRelease()
+  }, [])
+
+  return (
+    <div>
+      <h1>Release {release.id}</h1>
+      <ul>
+        <li>From {release.period_from}</li>
+        <li>To {release.period_to}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ReleaseDetail
