56a34a315197a3076396387701dac0478cd83b39
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-17 10:02:19 +0200

    Use Heading component from designsystem-react as an example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import { Heading } from '@digdir/designsystemet-react'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +18,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <Heading level={1} data-size="xl">Get started</Heading>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
