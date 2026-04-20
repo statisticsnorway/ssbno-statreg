c1507530fe7c100b64b3088aa80303fbf1f989d0
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-20 11:11:51 +0200

    fix: Use Divider component from designsystemet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import ReleaseDetail from './views/releaseDetail'
 import StatisticDetail from './views/statisticDetail'
 import ssbLogo from './assets/SSB_logo_black.svg'
 
+import { Divider } from '@digdir/designsystemet-react'
+
 {/* TODO: This is only placeholder; create own Header component if necessary */}
 const Header = () => (
   <div id="header">
     <div className="header-content">
       <img src={ssbLogo} className="logo" alt="SSB logo"  />
     </div>
+    <Divider />
   </div>
 )
 
@@ -19,7 +22,7 @@ function App() {
       <main id="page-content">
         <div className='content' data-color='brand1'>
           <ReleaseDetail></ReleaseDetail>
-          <hr />
+          <Divider />
           <StatisticDetail></StatisticDetail>
         </div>
       </main>
