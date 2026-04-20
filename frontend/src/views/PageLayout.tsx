b5c78c2112be9a816e7c9a74a7bec6c2944edd79
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-20 15:34:29 +0200

    fix: Remove unused classes

--- frontend/src/views/PageLayout.tsx
+++ frontend/src/views/PageLayout.tsx
@@ -6,7 +6,7 @@ import type { ReactNode } from 'react'
 {/* TODO: This is only placeholder; create own Header component if necessary */}
 const Header = () => (
   <div id="header">
-    <div className="header-content container">
+    <div className="header-content">
       <img src={ssbLogo} className="logo" alt="SSB logo"  />
       {/* TODO: Add links etc */}
     </div>
@@ -17,8 +17,8 @@ const PageLayout = ({ children }: {children: ReactNode}) => {
   return (
     <>
       <Header />
-      <main id="page-content">
-        <div className='content container'>
+      <main id="page-content" data-color='brand1'>
+        <div className='content'>
           {children}
         </div>
       </main>
