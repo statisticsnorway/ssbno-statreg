b2443ef7ac81d7b93c9c3f415206d7d373590fce
Glenruben Larsen <gle@ssb.no>
2026-04-23 14:33:59 +0200

    .js file ending is needed

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,4 @@
-export { StatisticStatus, ApprovalStatus } from './enums'
+export { StatisticStatus, ApprovalStatus } from './enums.js'
 import type { components } from './api-types'
 
 export type BlockedReleaseDate = components['schemas']['Blocked_release_date']
