7fcebec18465a5e6367d07277c6cc3413dadd5de
Anne-Siri Trønnes <ntr@ssb.no>
2026-04-24 14:12:31 +0200

    feat: added calendar_days endpoint and service

--- backend/src/api/controllers/calendarController.ts
+++ backend/src/api/controllers/calendarController.ts
@@ -1,13 +1,26 @@
 import { prisma } from '@/lib/prisma'
 import { handleErrors } from '@/lib/prismaErrors'
-import { createBlockedReleaseDay } from '@/services/calendarService'
+import { createBlockedReleaseDay, getDateStatusForRange } from '@/services/calendarService'
 import { Router } from 'express'
-import { requireAdminAuthorization } from 'plugins/authMiddleware'
+import { requireAdminAuthorization, skipAuth } from 'plugins/authMiddleware'
 
 export default function calendarController(router: Router) {
   router.post('/calendar/blocked-release-days/:date', requireAdminAuthorization(), async (req, res) => {
     try {
       const result = await createBlockedReleaseDay(prisma, req.params.date, req.body)
+      res.json(result)
+    } catch (error) {
+      handleErrors(error, res)
+    }
+  })
+
+  router.get('/calendar/:fromYear-:fromMonth-:fromDay-:toYear-:toMonth-:toDay', skipAuth, async (req, res) => {
+    try {
+      // TODO MIM-2661: Refactor and make sure errors are handled
+      const { fromYear, fromMonth, fromDay, toYear, toMonth, toDay } = req.params
+      const from = fromYear + '-' + fromMonth + '-' + fromDay
+      const to = toYear + '-' + toMonth + '-' + toDay
+      const result = await getDateStatusForRange(prisma, from, to)
       res.json(result)
     } catch (error) {
       handleErrors(error, res)
