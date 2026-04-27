c5f4c46a8fa6bc553335f45788225e73565952d6
Glenruben Larsen <gle@ssb.no>
2026-04-27 13:29:31 +0200

    remove unused comment

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,6 @@
 import createClient from 'openapi-fetch'
 import type { paths } from '../../shared/src/api-types'
 
-// TODO: Use BASE_URL instead; somehow it doesn't quite work
 const client = createClient<paths>({ baseUrl: '/statistikkregisteret/api' })
 
 export default client
