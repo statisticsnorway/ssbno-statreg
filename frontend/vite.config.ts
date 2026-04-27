6ba8ef4c12f14c43fb14e6c0f5c2a8f1c20ca853
Glenruben Larsen <gle@ssb.no>
2026-04-27 13:32:56 +0200

    fix proxy for all /api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// TODO: Fix proxy after routing strategy is decided
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,11 +8,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/statistikkregisteret/api/releases': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/statistikkregisteret/api/statistics': {
+      '/statistikkregisteret/api/': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
