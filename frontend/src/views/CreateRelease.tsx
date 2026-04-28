bed092f9b5db9affbac69435e75192a51a047c81
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-28 14:17:48 +0200

    feat: Start on implementing page for create releases

--- frontend/src/views/CreateRelease.tsx
+++ frontend/src/views/CreateRelease.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react'
+import { Heading, } from '@digdir/designsystemet-react'
+import { type ReleaseDetails } from '@ssbno-statreg/shared'
+import client from '../api'
+
 export default function CreateRelease() {
-  return (<h1>CreateRelease will be served here!</h1>)
+  const [releases, setReleases] = useState<ReleaseDetails>({})
+
+  useEffect(() => {
+    async function fetchRelease() {
+      const { data, error } = await client.GET('/statistics/{shortname}/variants/{id}/releases', { params: { path: { shortname: 'utlaerling', id: 9024 } } })
+      if (error) {
+        const errorMessage = (error as any).error
+        console.log(errorMessage)
+        alert(errorMessage)
+      } else {
+        setReleases(data)
+      }
+    }
+    fetchRelease()
+  }, [])
+
+
+  return (
+    <>
+      <Heading level={1} data-size='sm'>Meld publiseringsdato</Heading>
+      {console.log(releases)}
+    </>
+  )
 }
