d0e1f8e57a3fd755f70252a25663cc417a98545b
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-28 14:51:32 +0200

    fix: Use ReleaseTable component for ListReleases

--- frontend/src/views/ListReleases.tsx
+++ frontend/src/views/ListReleases.tsx
@@ -2,17 +2,14 @@ import { useState, useEffect } from 'react'
 import {
   Heading,
   Paragraph,
-  Table,
   Field,
   Label,
   Select,
-  Link,
   Pagination,
   usePagination,
 } from '@digdir/designsystemet-react'
 import { type ReleaseListing } from '@ssbno-statreg/shared'
-import { formatPublishTime, formatDate } from '../lib/utils'
-import { ApprovalStatusBadge } from '../components/ApprovalStatus'
+import { ReleasesTable } from '../components/ReleasesTable'
 
 import client from '../api'
 
@@ -49,54 +46,6 @@ function ListReleases() {
   function handleChangeShowRowCount(e: React.ChangeEvent<HTMLSelectElement>) {
     setShowRowCount(Number(e.target.value))
     setCurrentPage(1)
-  }
-
-  const tableHeaderCells = [
-    'Kortnavn',
-    'Statistikknavn',
-    'Variant',
-    'Måleperiodetittel',
-    'Målperiode fra',
-    'Måleperiode til',
-    'Publiseringsdato',
-    'Status',
-  ]
-
-  function renderListReleasesTableHeaderCells() {
-    return tableHeaderCells.map((header) => <Table.HeaderCell key={header}>{header}</Table.HeaderCell>)
-  }
-
-  const TruncatedTableCell = ({ value, maxWidth = '340px' }: { value: string | undefined; maxWidth?: string }) => (
-    <Table.Cell style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth }} title={value}>
-      {value}
-    </Table.Cell>
-  )
-
-  // TODO: MIM-2555: Add måleperiodetittel after logic is implemented
-  function renderListReleasesTableRows() {
-    return Object.entries(releases).map(([__, release]) => {
-      const statisticsShortname = release.statistic?.shortname ?? ''
-      return (
-        <Table.Row key={`${release.publish_time}-${release.id}`}>
-          <Table.Cell>
-            <Link href={`/statistikkregisteret/statistikk/${statisticsShortname}`}>{statisticsShortname}</Link> {/* TODO: Fix /statistikkregisteret urls with react-router; this applies to the /publisering link as well */}
-          </Table.Cell>
-          <TruncatedTableCell value={release.statistic?.name} />
-          <Table.Cell>{release.frequency?.name ?? ''}</Table.Cell>
-          <Table.Cell>TBA</Table.Cell>
-          <Table.Cell>{formatDate(release.period_from)}</Table.Cell>
-          <Table.Cell>{formatDate(release.period_to)}</Table.Cell>
-          <Table.Cell>
-            <Link href={`/statistikkregisteret/publisering/${release.id}`}>
-              {formatPublishTime(release.publish_time)}
-            </Link>
-          </Table.Cell>
-          <Table.Cell>
-            <ApprovalStatusBadge status={release.approval_status} />
-          </Table.Cell>
-        </Table.Row>
-      )
-    })
   }
 
   function renderShowRowCountSelect() {
@@ -158,12 +107,7 @@ function ListReleases() {
           >
             {renderShowRowCountSelect()}
           </div>
-          <Table>
-            <Table.Head>
-              <Table.Row>{renderListReleasesTableHeaderCells()}</Table.Row>
-            </Table.Head>
-            <Table.Body>{renderListReleasesTableRows()}</Table.Body>
-          </Table>
+          <ReleasesTable releases={releases} />
           {renderListReleasesTablePagination()}
         </div>
       </>
