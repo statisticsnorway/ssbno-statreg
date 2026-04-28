272e88b7243ad1b0e12119e3cb486e7d7d22c6ef
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-28 12:54:21 +0200

    Add TODO

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -19,6 +19,7 @@ export type StatisticUpdate = components['schemas']['Statistic_update']
 export type StatisticListing = components['schemas']['Statistic_listing']
 export type Variant = components['schemas']['Variant']
 
+// TODO: Remove workaround for CalendarDateStatus and CalendarDates after MIM-2661 has been merged
 export type CalendarDateStatus = 'blocked' | 'free' | 'few' | 'more' | 'full'
 
 export type CalendarDates = {
