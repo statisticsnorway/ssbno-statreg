db3c2047468e1853cf6e50d77f2713b407d1f31b
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2026-04-29 05:17:46 -0700

    feat: Add color coding explanation to DatePicker component [MIM-2656] (#254)
    * feat: Add color-coding explanation for DatePicker component
    
    * fix: Design tweaks
    
    * fix: Add TODO
    
    * refactor: Fix types and redundant code
    
    * refactor: Rename prop for color coding
    
    * Update frontend/src/components/DatePicker.tsx
    
    Co-authored-by: Anne-Siri <142512638+annesiri@users.noreply.github.com>
    
    * fix: more -> many and fix styling after merge
    
    ---------
    
    Co-authored-by: Anne-Siri <142512638+annesiri@users.noreply.github.com>

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -2,10 +2,12 @@ import '@navikt/ds-css/dist/global/tokens.css'
 import '@navikt/ds-css/dist/components.css'
 
 import { DatePicker as AkselDatePicker } from '@navikt/ds-react/DatePicker'
-import type { CalendarDates } from '@ssbno-statreg/shared'
+import { type CalenderDate, DayStatus } from '@ssbno-statreg/shared'
+import { Paragraph } from '@digdir/designsystemet-react'
 
 type DatePickerProps = React.ComponentProps<typeof AkselDatePicker.Standalone> & {
-  calendarDates: CalendarDates
+  calendarDates: CalenderDate
+  showColorCodingExplanation?: boolean
 }
 
 function parseDate(dateString: string): Date {
@@ -13,41 +15,75 @@ function parseDate(dateString: string): Date {
   return new Date(year, month - 1, day)
 }
 
-export function DatePicker({ calendarDates, ...props }: DatePickerProps) {
+export function DatePicker({ calendarDates, showColorCodingExplanation, ...props }: DatePickerProps) {
   const full: Date[] = []
-  const more: Date[] = []
+  const many: Date[] = []
   const few: Date[] = []
   const blocked: Date[] = []
 
-  for (const [dateString, value] of Object.entries(calendarDates)) {
+  for (const [dateString, value] of Object.entries(calendarDates) as [string, CalenderDate[string]][]) {
     const date = parseDate(dateString)
     if (value.status === 'full') full.push(date)
-    else if (value.status === 'more') more.push(date)
+    else if (value.status === 'many') many.push(date)
     else if (value.status === 'few') few.push(date)
     else if (value.status === 'blocked') blocked.push(date)
   }
 
+  const statusColors: Record<keyof typeof DayStatus, { backgroundColor: string }> = {
+    FULL: { backgroundColor: 'var(--ds-color-danger-base-default)' },
+    MANY: { backgroundColor: 'var(--ds-color-warning-base-default)' },
+    FEW: { backgroundColor: 'var(--ds-color-base-default)' }, // TODO: Our base-default color is green due to the theme
+    BLOCKED: { backgroundColor: 'var(--ds-color-neutral-border-subtle)' },
+    NONE: { backgroundColor: 'var(--ds-color-base-background)' },
+  }
+
   const sharedStyles = {
-    color: 'white', 
+    color: 'white',
     borderRadius: '8px',
   }
-  
+
   return (
-    <AkselDatePicker.Standalone
-      // @ts-expect-error: Allow custom "modifiers" prop for color coding
-      modifiers={{ full, more, few, blocked }}
-      modifiersStyles={{
-        full: { backgroundColor: 'var(--ds-color-danger-base-default)', ...sharedStyles },
-        more: { backgroundColor: 'var(--ds-color-warning-base-default)', ...sharedStyles },
-        few: { backgroundColor: 'var(--ds-color-success-base-default)', ...sharedStyles },
-        blocked: { backgroundColor: 'var(--ds-color-neutral-border-subtle)', textDecoration: 'line-through', ...sharedStyles }
-      }}
-      style={{
-        padding: '20px 16px',
-        boxShadow: 'var(--ds-shadow-md)',
-        borderRadius: '8px',
-      }}
-      {...props}
-    />
+    <div style={{ display: 'flex', flexDirection: 'row', gap: 'var(--ds-size-10)', alignItems: 'center' }}>
+      <AkselDatePicker.Standalone
+        // @ts-expect-error: Allow custom "modifiers" prop for color coding
+        modifiers={{ full, many, few, blocked }}
+        modifiersStyles={{
+          full: { ...statusColors.FULL, ...sharedStyles },
+          many: { ...statusColors.MANY, ...sharedStyles },
+          few: { ...statusColors.FEW, ...sharedStyles },
+          blocked: {
+            ...statusColors.BLOCKED,
+            ...sharedStyles,
+            textDecoration: 'line-through',
+          },
+        }}
+        style={{
+          padding: '20px 16px',
+          boxShadow: 'var(--ds-shadow-md)',
+          borderRadius: '8px',
+        }}
+        {...props}
+      />
+
+      {showColorCodingExplanation && (
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--ds-size-2)' }}>
+          {Object.entries(DayStatus).map(([key, value]) => (
+            <div key={key} style={{ display: 'flex', alignItems: 'center', gap: 'var(--ds-size-4)' }}>
+              <div
+                style={{
+                  display: 'inline-block',
+                  width: '30px',
+                  height: '30px',
+                  borderRadius: '3px',
+                  ...(key === 'NONE' ? { border: '1px solid black' } : {}),
+                  ...statusColors[key as keyof typeof statusColors],
+                }}
+              />
+              <Paragraph data-size='xs'>{value}</Paragraph>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
