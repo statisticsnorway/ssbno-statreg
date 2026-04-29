6a626689791a1b643f6b8a448d3972fe23995e7b
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-29 08:00:18 +0200

    fix: Add Headings and status for create release page

--- frontend/src/views/CreateRelease.tsx
+++ frontend/src/views/CreateRelease.tsx
@@ -3,12 +3,14 @@ import { Heading, Tabs } from '@digdir/designsystemet-react'
 import { CalendarIcon } from '@navikt/aksel-icons'
 import { type ReleaseListing } from '@ssbno-statreg/shared'
 
-import client from '../api'
 import { ReleasesTable } from '../components/ReleasesTable'
+import { ApprovalStatusTag } from '../components/ApprovalStatus'
+
+import client from '../api'
 
 export default function CreateRelease() {
   const [releases, setReleases] = useState<ReleaseListing[]>([])
-
+  
   useEffect(() => {
     async function fetchRelease() {
       const { data, error } = await client.GET('/statistics/{shortname}/variants/{id}/releases', {
@@ -25,8 +27,12 @@ export default function CreateRelease() {
     fetchRelease()
   }, [])
 
+  const release = Object.entries(releases).map(([__, release]) => release)[0]
+  const statisticShortname = release.statistic.shortname
+  const variant = release.frequency.name
+  const approvalStatus = release.approval_status
+
   function renderReleasesTables() {
-    const release = Object.entries(releases).map(([__, release]) => release)[0]
     return (
       <Tabs defaultValue='selected-publish-date' style={{ width: '100%', rowGap: 'var(--ds-size-10)' }}>
         <Tabs.List style={{ marginBottom: 'var(--ds-size-10)' }}>
@@ -35,7 +41,7 @@ export default function CreateRelease() {
             Publiseringer på valgt dato
           </Tabs.Tab>
           <Tabs.Tab value='all-releases'>
-            Alle publiseringer på {release?.statistic?.shortname}, {release?.frequency?.name}
+            Alle publiseringer på {statisticShortname}, {variant}
           </Tabs.Tab>
         </Tabs.List>
         <Tabs.Panel style={{ padding: '0' }} value='selected-publish-date'> {/* TODO: Padding can be set with classes instead of inline-css */}
@@ -50,10 +56,15 @@ export default function CreateRelease() {
 
   return (
     <>
-      <Heading level={1} data-size='sm'>
-        Meld publiseringsdato
-      </Heading>
-
+      <div>
+        <Heading level={1} data-size='md'>
+          Meld publiseringsdato
+        </Heading>
+        <Heading level={2} data-size='xs'>
+          {release.statistic.name} ({statisticShortname}) og {variant}
+        </Heading>
+        {approvalStatus && <ApprovalStatusTag status={approvalStatus} />}
+      </div>
       {renderReleasesTables()}
     </>
   )
