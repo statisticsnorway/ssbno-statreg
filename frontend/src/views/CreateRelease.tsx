ef616cc3542e3bcd4e0e4ca2ce1545beafbb790e
johnnadeluy <johnnadel.salita@gmail.com>
2026-04-29 07:49:45 +0200

    feat: Add Tabs for CreateRelease Table

--- frontend/src/views/CreateRelease.tsx
+++ frontend/src/views/CreateRelease.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { Heading } from '@digdir/designsystemet-react'
+import { Heading, Tabs } from '@digdir/designsystemet-react'
+import { CalendarIcon } from '@navikt/aksel-icons'
 import { type ReleaseListing } from '@ssbno-statreg/shared'
 
 import client from '../api'
@@ -24,12 +25,36 @@ export default function CreateRelease() {
     fetchRelease()
   }, [])
 
+  function renderReleasesTables() {
+    const release = Object.entries(releases).map(([__, release]) => release)[0]
+    return (
+      <Tabs defaultValue='selected-publish-date' style={{ width: '100%', rowGap: 'var(--ds-size-10)' }}>
+        <Tabs.List style={{ marginBottom: 'var(--ds-size-10)' }}>
+          <Tabs.Tab value='selected-publish-date'>
+            <CalendarIcon />
+            Publiseringer på valgt dato
+          </Tabs.Tab>
+          <Tabs.Tab value='all-releases'>
+            Alle publiseringer på {release?.statistic?.shortname}, {release?.frequency?.name}
+          </Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel style={{ padding: '0' }} value='selected-publish-date'> {/* TODO: Padding can be set with classes instead of inline-css */}
+          Innmeldte datoer den ...
+        </Tabs.Panel>
+        <Tabs.Panel style={{ padding: '0' }} value='all-releases'>
+          <ReleasesTable releases={releases} /> {/* TODO: Include pagination and x row selection to component */}
+        </Tabs.Panel>
+      </Tabs>
+    )
+  }
+
   return (
     <>
       <Heading level={1} data-size='sm'>
         Meld publiseringsdato
       </Heading>
-      <ReleasesTable releases={releases} />
+
+      {renderReleasesTables()}
     </>
   )
 }
