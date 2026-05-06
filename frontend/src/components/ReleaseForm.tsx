9313b93bc7453e3174458b383f95170f61126dd9
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2026-05-06 03:37:23 -0700

    feat: Add validation components for Create Release Form [MIM-2665] (#266)
    * feat: Add validation components for Create Release Form
    
    * fix: wip Use ValidationMessage component with aria-invalid
    
    * fix: Use correct Date formats for input
    
    * fix: Update errors onChange
    
    * fix: wip Add period to and from comparison error
    
    * test: Add unit tests for getDateOnlyString
    
    * fix: Remove if-check on useEffect for DatePicker
    
    * fix: Fix ErrorSummary links
    
    * refactor: Reuse getDateOnlyAsString utils function
    
    * refactor: wip Use one state for form values
    
    * refactor: Add TODO
    
    * fix: Fix periodFrom and periodTo validation errors
    
    * fix: Remove if-checks for DatePicker onDateChange hook
    
    * fix: Remove conditionals in DatePicker onChange

--- frontend/src/components/ReleaseForm.tsx
+++ frontend/src/components/ReleaseForm.tsx
@@ -1,26 +1,112 @@
 import { useState } from 'react'
-import { Paragraph, Select, Button, Fieldset, Field, Label, Input } from '@digdir/designsystemet-react'
+import {
+  Paragraph,
+  Select,
+  Button,
+  Fieldset,
+  Field,
+  Label,
+  Input,
+  ValidationMessage,
+  ErrorSummary,
+} from '@digdir/designsystemet-react'
 import { DatePicker as AkselDatePicker, useDatepicker } from '@navikt/ds-react/DatePicker'
 import { DatePicker } from './DatePicker'
-import { getFirstDayOfNthMonth, getLastDayOfNthMonth } from '../lib/utils'
+import { getDateOnlyAsString, getFirstDayOfNthMonth, getLastDayOfNthMonth } from '../lib/utils'
 
 const releaseDatePrecisions = ['Dag', 'Måned', 'År']
 
-export function ReleaseForm() {
-  const [dateType, setDateType] = useState('')
-  const { datepickerProps: publishTimePickerProps, inputProps: publishTimeInputProps } = useDatepicker()
-  const { datepickerProps: periodFromPickerProps, inputProps: periodFromInputProps } = useDatepicker()
-  const { datepickerProps: periodToPickerProps, inputProps: periodToInputProps } = useDatepicker()
+type ReleaseFormTypes = {
+  dateType?: string
+  publishTime?: string
+  periodFrom?: string
+  periodTo?: string
+}
 
-  const start = getFirstDayOfNthMonth(0)
-  const stop = getLastDayOfNthMonth(0)
+export function ReleaseForm() {
+  const [values, setValues] = useState<ReleaseFormTypes>({
+    dateType: '',
+    publishTime: '',
+    periodFrom: '',
+    periodTo: '',
+  })
+  const [periodToDate, setPeriodToDate] = useState<Date | undefined>()
+  const [periodFromDate, setPeriodFromDate] = useState<Date | undefined>()
+  const [errors, setErrors] = useState<ReleaseFormTypes>({
+    dateType: '',
+    publishTime: '',
+    periodFrom: '',
+    periodTo: '',
+  })
+
+  // TODO: Add validation for Date inputs; it's currently allowed to write anything on the fields
+  const { datepickerProps: publishTimePickerProps, inputProps: publishTimeInputProps } = useDatepicker({
+    onDateChange: (publishTime) => {
+      setValues((values) => ({ ...values, publishTime: getDateOnlyAsString(publishTime) }))
+      setErrors((errors) => ({ ...errors, publishTime: '' }))
+    },
+  })
+  const { datepickerProps: periodFromPickerProps, inputProps: periodFromInputProps } = useDatepicker({
+    onDateChange: (periodFrom) => {
+      setPeriodFromDate(periodFrom)
+      setValues((values) => ({ ...values, periodFrom: getDateOnlyAsString(periodFrom) }))
+      setErrors((errors) => ({ ...errors, periodFrom: '' }))
+    },
+  })
+  const { datepickerProps: periodToPickerProps, inputProps: periodToInputProps } = useDatepicker({
+    onDateChange: (periodTo) => {
+      setPeriodToDate(periodTo)
+      setValues((values) => ({ ...values, periodTo: getDateOnlyAsString(periodTo) }))
+      setErrors((errors) => ({ ...errors, periodTo: '' }))
+    },
+  })
+
+  function validateFields() {
+    const nextErrors: ReleaseFormTypes = {}
+
+    if (!values.dateType) nextErrors.dateType = 'Velg en datotype for publisering'
+    if (!values.publishTime) nextErrors.publishTime = 'Velg en publiseringsdato'
+    if (!values.periodFrom) nextErrors.periodFrom = 'Velg en fra-dato'
+    if (!values.periodTo) nextErrors.periodTo = 'Velg en til-dato'
+
+    // TODO: Review error messages and implement onChange
+    if (periodFromDate && periodToDate && periodFromDate > periodToDate) {
+      nextErrors.periodFrom = 'Fra-dato kan ikke være etter til-dato'
+      nextErrors.periodTo = 'Til-dato kan ikke være før fra-dato'
+    }
+
+    setErrors(nextErrors)
+    return Object.keys(nextErrors).some((key) => nextErrors[key as keyof ReleaseFormTypes]) ? false : true
+  }
+
+  function handleOnSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    if (!validateFields()) return
+
+    // TODO: Replace with POST logic
+    console.log({
+      dateType: values.dateType,
+      publishTime: values.publishTime,
+      periodFrom: values.periodFrom,
+      periodTo: values.periodTo,
+    })
+  }
 
   return (
-    <>
+    <form onSubmit={handleOnSubmit} style={{ display: 'flex', flexDirection: 'column', gap: 'var(--ds-size-12)' }}>
       <Field>
         <Paragraph style={{ marginBottom: 'var(--ds-size-8)' }}>Alle felter må fylles ut</Paragraph>
         <Label>Datotype for publisering</Label>
-        <Select value={dateType} onChange={(e) => setDateType(e.target.value)}>
+        <Select
+          id='dateType'
+          value={values.dateType}
+          onChange={(e) => {
+            setValues((values) => ({ ...values, dateType: e.target.value }))
+            setErrors((errors) => ({ ...errors, dateType: '' }))
+          }}
+          aria-invalid={!!errors.dateType}
+        >
           <Select.Option value='' disabled>
             Velg datotype
           </Select.Option>
@@ -30,16 +116,23 @@ export function ReleaseForm() {
             </Select.Option>
           ))}
         </Select>
+        {errors.dateType && <ValidationMessage>{errors.dateType}</ValidationMessage>}
       </Field>
 
       <Field>
         <Label>Publiseringsdato</Label>
         <Field.Description>
           Nye datoer og endringer må meldes minst 3 måneder i forveien. <br />
-          For kortere frister, kontakt desken@ssb.no.
+          For kortere frister, kontakt mmj@ssb.no.
         </Field.Description>
-        <Input size={10} {...publishTimeInputProps} />
-        <DatePicker fromDate={start} toDate={stop} showColorCodingExplanation {...publishTimePickerProps} />
+        <Input id='publishTime' size={10} {...publishTimeInputProps} aria-invalid={!!errors.publishTime} />
+        <DatePicker
+          fromDate={getFirstDayOfNthMonth(0)}
+          toDate={getLastDayOfNthMonth(0)}
+          showColorCodingExplanation
+          {...publishTimePickerProps}
+        />
+        {errors.publishTime && <ValidationMessage>{errors.publishTime}</ValidationMessage>}
       </Field>
 
       <Fieldset>
@@ -47,22 +140,47 @@ export function ReleaseForm() {
           <Field>
             <Label>Måleperiode fra</Label>
             <AkselDatePicker {...periodFromPickerProps}>
-              <AkselDatePicker.Input style={{ padding: '0' }} {...periodFromInputProps} label />
+              <AkselDatePicker.Input
+                id='periodFrom'
+                {...periodFromInputProps}
+                aria-invalid={!!errors.periodFrom}
+                label
+              />
             </AkselDatePicker>
+            {errors.periodFrom && <ValidationMessage>{errors.periodFrom}</ValidationMessage>}
           </Field>
+
           <Field>
             <Label>Måleperiode til</Label>
             <AkselDatePicker {...periodToPickerProps}>
-              <AkselDatePicker.Input style={{ padding: '0' }} {...periodToInputProps} label />
+              <AkselDatePicker.Input id='periodTo' {...periodToInputProps} aria-invalid={!!errors.periodTo} label />
             </AkselDatePicker>
+            {errors.periodTo && <ValidationMessage>{errors.periodTo}</ValidationMessage>}
           </Field>
         </div>
       </Fieldset>
 
       <div style={{ display: 'flex', gap: 'var(--ds-size-3)' }}>
-        <Button>Meld dato</Button>
+        <Button type='submit'>Meld dato</Button>
         <Button variant='tertiary'>Avbryt</Button>
       </div>
-    </>
+
+      {Object.values(errors).some(Boolean) && (
+        <ErrorSummary>
+          <ErrorSummary.Heading>For å gå videre må du rette opp følgende feil:</ErrorSummary.Heading>
+          <ErrorSummary.List>
+            {Object.entries(errors).map(([key, message]) => {
+              if (message) {
+                return (
+                  <ErrorSummary.Item key={message}>
+                    <ErrorSummary.Link href={`#${key}`}>{message}</ErrorSummary.Link>
+                  </ErrorSummary.Item>
+                )
+              }
+            })}
+          </ErrorSummary.List>
+        </ErrorSummary>
+      )}
+    </form>
   )
 }
