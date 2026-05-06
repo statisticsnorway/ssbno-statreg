4ce93ccfde572c513a7b4d69d0b9992bd4ad0235
Anne-Siri Trønnes <ntr@ssb.no>
2026-05-06 14:36:39 +0200

    fix base url

--- backend/test/integration/calendar.test.ts
+++ backend/test/integration/calendar.test.ts
@@ -1,20 +1,19 @@
 import { describe, expect, test } from 'vitest'
 
-const BASE_URL = process.env.API_URL ?? 'http://localhost:8080/statistikkregisteret/api/calendar'
+const BASE_URL = process.env.API_URL ?? 'http://localhost:8080/statistikkregisteret/api'
 
 const headers = {
   'Content-Type': 'application/json',
 }
 describe('calendarController ', () => {
   test('creates blocked date when client posts', async () => {
-    const response = await fetch(`${BASE_URL}/blocked-release-days/2026-05-15`, {
+    const response = await fetch(`${BASE_URL}/calendar/blocked-release-days/2026-05-15`, {
       method: 'POST',
       headers: headers,
       body: JSON.stringify({ blocked_comment: 'Inneklemt dag' }),
     })
     expect(response.status).toBe(200)
     const json = await response.json()
-    console.log(json)
     expect(json).toContainEqual({
       blocked_comment: 'Inneklemt dag',
       date: '2026-05-15',
@@ -22,7 +21,7 @@ describe('calendarController ', () => {
   })
 
   test('retrieves date status for days in range', async () => {
-    const response = await fetch(`${BASE_URL}?fromDate=2026-05-25&toDate=2026-05-31`)
+    const response = await fetch(`${BASE_URL}/calendar?fromDate=2026-05-25&toDate=2026-05-31`)
     expect(response.status).toBe(200)
     const list = await response.json()
     expect(list).toStrictEqual({
