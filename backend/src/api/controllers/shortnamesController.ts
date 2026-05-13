38443ef5fb05d8517ac867b19ef4763421470868
Anne-Siri Trønnes <ntr@ssb.no>
2026-05-13 13:59:09 +0200

    feat: add getShortnames()

--- backend/src/api/controllers/shortnamesController.ts
+++ backend/src/api/controllers/shortnamesController.ts
@@ -1,11 +1,13 @@
 import { handleErrors } from '@/lib/prismaErrors'
+import { getShortnames } from '@/services/shortnamesService'
 import { Router } from 'express'
 import { skipAuth } from 'plugins/authMiddleware'
+import { prisma } from '@/lib/prisma'
 
 export default function shortnamesController(router: Router) {
-  router.get('/shortnames', skipAuth, async (req, res) => {
+  router.get('/shortnames', skipAuth, async (_req, res) => {
     try {
-      const result = ['kpi', 'energi', 'befolkning']
+      const result = await getShortnames(prisma)
       res.json(result)
     } catch (error) {
       handleErrors(error, res)
