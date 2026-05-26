435df4c41ba526042722f441db75d51efed35b72
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2026-05-26 01:33:45 -0700

    feat: Implement sort by publish time for frontend [MIM-2711] (#303)
    * refactor: Split getReleases to multiple functions [MIM-2710]
    
    * test: Start adding unit tests for getVariantRelease and getFilteredReleases
    
    * test: Add undefined filterByShortname test for getFilteredReleases
    
    * fix: Require both shortname and variantId for buildVariantReleaseFilter after refactor
    
    * Minor code refactoring
    
    * refactor: Destructure instead
    
    * refactor: Refactor buildVariantReleaseFilter also
    
    * feat: Implement sort by publish_time on backend [MIM-2711]
    
    * refactor: Add orderBy fallback for releases
    
    * fix: Use Set for sorting fields
    
    * refactor: Remove unecessary type casting
    
    * test: Add unit tests for ensureStringArray utils function
    
    * fix: Remove undefined test for ensureStringArray
    
    * refactor: Add missing tests and remove unecessary code
    
    * refactor: Set mocked assert results to true by default
    
    * feat: Start on sort functionality for ReleaseTable
    
    * refactor
    
    * fix: Sort can be undefined when param is not defined
    
    * test: Add test for sort
    
    * test: Add test for invalid_field insertion for sort query
    
    * fix: Implement logic for Release Table sort toggle
    
    * fix: Support multi-sort in toggleSort logic
    
    * refactor: Remove duplicate types; use intersected types
    
    * refactor: Move types at the top of the file
    
    * refactor: More readable toggleSort logic
    
    * docs: Add explanatory examples for sort logic

--- frontend/src/views/ReleaseForm.tsx
+++ frontend/src/views/ReleaseForm.tsx
@@ -415,10 +415,13 @@ function getCreatedReleaseModalDescription(createdRelease: ReleaseDetails) {
 // TODO should take a date prop MIM-1740
 function DateReleasesTable() {
   const [releases, setReleases] = useState<ReleaseListing[]>([])
+  const [sortBy, setSortBy] = useState<string[]>([])
 
   useEffect(() => {
     async function fetchReleases() {
-      const { data, error } = await client.GET('/releases', { params: { query: { start: 0, count: 10 } } })
+      const { data, error } = await client.GET('/releases', {
+        params: { query: { start: 0, count: 10, sort: sortBy.join(',') } },
+      })
       if (error) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const errorMessage = (error as any).error
@@ -429,9 +432,9 @@ function DateReleasesTable() {
       }
     }
     fetchReleases()
-  }, [])
+  }, [sortBy])
 
-  return <ReleasesTable releases={releases} />
+  return <ReleasesTable releases={releases} sortBy={sortBy} setSortBy={setSortBy} />
 }
 
 function VariantReleasesTable({ shortname, variantId }: { shortname: string; variantId: number }) {
@@ -439,11 +442,12 @@ function VariantReleasesTable({ shortname, variantId }: { shortname: string; var
   const [start, setStart] = useState(0)
   const [releases, setReleases] = useState<ReleaseListing[]>([])
   const [total, setTotal] = useState(0)
+  const [sortBy, setSortBy] = useState<string[]>([])
 
   useEffect(() => {
     async function fetchVariantReleases() {
       const { data, error } = await client.GET('/statistics/{shortname}/variants/{id}/releases', {
-        params: { path: { shortname, id: variantId }, query: { start, count } },
+        params: { path: { shortname, id: variantId }, query: { start, count, sort: sortBy.join(',') } },
       })
       if (error) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -456,7 +460,7 @@ function VariantReleasesTable({ shortname, variantId }: { shortname: string; var
       }
     }
     fetchVariantReleases()
-  }, [shortname, variantId, count, start])
+  }, [shortname, variantId, count, start, sortBy])
 
   function updateRowCount(newCount: number) {
     setCount(newCount)
@@ -478,6 +482,8 @@ function VariantReleasesTable({ shortname, variantId }: { shortname: string; var
         total={total}
         releases={releases}
         setCurrentPage={setCurrentPage}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
       />
     </>
   )
