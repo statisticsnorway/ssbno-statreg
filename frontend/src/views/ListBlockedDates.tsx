a91b4a154aa1f8dfd816687aa2e39cda6223a10a
Olav Landsverk <stud-oll@ssb.no>
2026-06-01 11:28:16 +0200

    add header
    Signed-off-by: Olav Landsverk <stud-oll@ssb.no>

--- frontend/src/views/ListBlockedDates.tsx
+++ frontend/src/views/ListBlockedDates.tsx
@@ -1,3 +1,19 @@
+import { Heading, Link, Paragraph } from '@digdir/designsystemet-react'
+import { ArrowLeftIcon } from '@navikt/aksel-icons'
+import { Link as ReactRouterLink } from 'react-router'
+
 export default function ListBlockedDates() {
-  return <h1>ListBlockedDates will be served here!</h1>
+  return (
+    <>
+      <Link asChild>
+        <ReactRouterLink to='/publisering'>
+          <ArrowLeftIcon /> Tilbake til publiseringsoversikten
+        </ReactRouterLink>
+      </Link>
+      <div>
+        <Heading data-size='sm' style={{ marginBottom: 'var(--ds-size-4)' }}>Sperrede datoer</Heading>
+        <Paragraph>Datoer som er automatisk lagt inn kan ikke redigeres eller slettes</Paragraph>
+      </div>
+    </>
+  )
 }
