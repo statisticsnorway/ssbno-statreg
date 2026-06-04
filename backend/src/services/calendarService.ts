d923b00920795a4659570874f043abe5afcf5421
Olav Landsverk <stud-oll@ssb.no>
2026-06-04 14:53:53 +0200

    use currentDate
    Signed-off-by: Olav Landsverk <stud-oll@ssb.no>

--- backend/src/services/calendarService.ts
+++ backend/src/services/calendarService.ts
@@ -24,7 +24,7 @@ export async function getFutureBlockedReleaseDates(
   const prismaResult = await prisma.calender_date.findMany({
     where: {
       day: {
-        gt: new Date(),
+        gt: currentDate,
       },
     },
     select: { comment: true, day: true },
