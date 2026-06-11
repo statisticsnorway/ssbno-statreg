fdaf29df619d738b6902b746d51a49e09bfdc55d
Glenruben <gle@ssb.no>
2026-06-11 14:17:18 +0200

    Add authentication check to openApi (#336)
    * Add authentication check to openApi
    
    * correct patronym
    
    * Add comment with ticket ID
    
    * forgot api-types
    
    * add back comment about auth/me after merge conflict resolution

--- shared/src/api-types.ts
+++ shared/src/api-types.ts
@@ -707,6 +707,49 @@ export interface paths {
     patch?: never
     trace?: never
   }
+  '/auth/authenticate': {
+    parameters: {
+      query?: never
+      header?: never
+      path?: never
+      cookie?: never
+    }
+    /** Check authentication status and information for current user. Used for controlling front-end behaviour, not security. */
+    get: {
+      parameters: {
+        query?: never
+        header?: never
+        path?: never
+        cookie?: never
+      }
+      requestBody?: never
+      responses: {
+        /** @description Object with admin-status, user info */
+        200: {
+          headers: {
+            [name: string]: unknown
+          }
+          content: {
+            'application/json': {
+              /** @example true */
+              isAdmin?: boolean
+              /** @example alice@example.com */
+              email?: string
+              /** @example Alice Examplesdottir */
+              fullName?: string
+            }
+          }
+        }
+      }
+    }
+    put?: never
+    post?: never
+    delete?: never
+    options?: never
+    head?: never
+    patch?: never
+    trace?: never
+  }
 }
 export type webhooks = Record<string, never>
 export interface components {
