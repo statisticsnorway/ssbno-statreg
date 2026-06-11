1c4f63db958ea9433e2a181e6170f1552796ddd1
Olav Landsverk <stud-oll@ssb.no>
2026-06-11 11:04:58 +0200

    import changes
    Signed-off-by: Olav Landsverk <stud-oll@ssb.no>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,13 +3,13 @@ import express from 'express'
 import helmet from 'helmet'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yaml'
+import { type JWTPayload } from 'jose'
 import { requireAuthorization } from '../plugins/authMiddleware'
 import { promBundleMetrics } from '../plugins/promBundle'
 import controllerRouter from './api/core/controllerRouter'
 import { prisma } from './lib/prisma'
 import { initializeDepartments } from './services/klassService'
 import * as dotenv from 'dotenv'
-
 dotenv.config()
 
 export async function createApp() {
@@ -19,13 +19,33 @@ export async function createApp() {
   app.use(promBundleMetrics)
   app.use(express.json())
   const swaggerDocument = YAML.parse(fs.readFileSync('../shared/openapi/openapi.yaml', 'utf8'))
+
   app.use('/statistikkregisteret/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  app.get('/statistikkregisteret/auth/me', auth, (req, res) => {
+
+  app.get('/statistikkregisteret/api/auth/me', auth, (req, res) => {
     // For local testing, add requireUserAuthentication here
     res.json({
       claims: req.auth?.claims,
       username: req.auth?.username,
       email: req.auth?.email,
+    })
+  })
+
+  app.get('/statistikkregisteret/api/auth/authenticate', auth, (req, res) => {
+    const claims = req.auth?.claims as JWTPayload & {
+      dapla?: {
+        groups?: string[]
+      }
+    }
+
+    const claimGroups = claims.dapla?.groups
+
+    const adminGroups = process.env.ADMIN_GROUPS?.split(',') ?? []
+
+    res.json({
+      isAdmin: adminGroups.some((group) => claimGroups?.includes(group)),
+      email: req.auth?.email,
+      fullName: claims.name,
     })
   })
 
