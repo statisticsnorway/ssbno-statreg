66e0af91c09dff1c5f8b34b0d5d2e0f2bc914233
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2026-06-12 13:20:33 +0200

    fix: Fix edit button text based on role for Release Form [MIM-2788] (#343)
    * fix: Fix edit button text based on role for Release Form [MIM-2788]
    
    * refactor: Add if conditionals for submit button text

--- frontend/src/views/ReleaseForm.tsx
+++ frontend/src/views/ReleaseForm.tsx
@@ -43,6 +43,7 @@ import {
 import client from '../api'
 
 import './ReleaseForm.css'
+import { useAuth } from '../context/AuthContext'
 
 type Statistic = ReleaseByIdResponse['statistic'] & {
   approval_status?: ReleaseByIdResponse['approval_status']
@@ -84,7 +85,6 @@ function useDatepicker(
   })
 }
 
-// common release form for creating and editing release
 export default function ReleaseForm() {
   // for creation, path is /statistikk/:shortname/:variantId/opprett
   // for editing, path is /publisering/:id/rediger
@@ -106,6 +106,17 @@ export default function ReleaseForm() {
   const [openReleaseModal, setOpenReleaseModal] = useState(false)
   const [newOrUpdatedRelease, setNewOrUpdatedRelease] = useState<ReleaseDetails>({})
   const [calendarDates, setCalendarDates] = useState<CalenderDate>({})
+
+  const { auth } = useAuth()
+
+  let submitButtonText = 'Meld dato'
+  if (isEditing) {
+    submitButtonText = 'Lagre og godkjenn'
+
+    if (!auth?.isAdmin) {
+      submitButtonText = 'Send endringsforslag'
+    }
+  }
 
   // when id exists in url-path, fetch release and prefill form
   useEffect(() => {
@@ -136,7 +147,6 @@ export default function ReleaseForm() {
     // eslint-disable-next-line @eslint-react/exhaustive-deps
   }, [releaseId])
 
-  // when shortname and variantId exists in url-path, only fetch statistic and variant data
   useEffect(() => {
     async function fetchVariant() {
       if (!shortname || !variantId) return
@@ -335,7 +345,7 @@ export default function ReleaseForm() {
         )}
 
         <div className='release-form-button-wrapper'>
-          <Button type='submit'>{isEditing ? 'Lagre' : 'Meld dato'}</Button>
+          <Button type='submit'>{submitButtonText}</Button>
           <Button variant='tertiary' asChild>
             <ReactRouterLink
               to={isEditing ? `/publisering/${releaseId}` : `/statistikk/${statistic?.shortname}`}
