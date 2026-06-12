70cb1b8e6b86e3e730e9a75268cd83b6e4284514
johnnadeluy <johnnadel.salita@gmail.com>
2026-06-12 11:30:54 +0200

    feat: Add Mine saker button to Header component

--- frontend/src/layouts/PageLayout.tsx
+++ frontend/src/layouts/PageLayout.tsx
@@ -2,23 +2,31 @@ import './PageLayout.css'
 import ssbLogo from '../assets/SSB_logo_black.svg'
 
 import { Outlet } from 'react-router'
-import { Link } from '@digdir/designsystemet-react'
+import { Link, Button, Avatar } from '@digdir/designsystemet-react'
+import { useAuth } from '../context/AuthContext'
 
 {
   /* TODO: This is only placeholder; create own Header component if necessary */
 }
-const Header = () => (
-  <div id='header'>
-    <div className='header-content' data-color='brand1' data-size='sm'>
-      <img src={ssbLogo} className='logo' alt='SSB logo' />
-      <div className='header-links'>
-        {/* TODO: Placeholder links */}
-        <Link href='/statistikkregisteret/'>Publisering</Link>
-        <Link href='/statistikkregisteret/statistikk'>Statistikker</Link>
+const Header = () => {
+  const { auth } = useAuth()
+  const initials = auth?.email?.split('@')[0]
+
+  return (
+    <div id='header'>
+      <div className='header-content' data-color='brand1' data-size='sm'>
+        <img src={ssbLogo} className='logo' alt='SSB logo' />
+        <div className='header-links'>
+          <Link href='/statistikkregisteret/'>Publisering</Link>
+          <Link href='/statistikkregisteret/statistikk'>Statistikker</Link>
+          <Button variant='tertiary' onClick={() => alert('Kommer senere')}>
+            Mine saker <Avatar aria-label={auth?.fullName ?? ''} initials={initials} />
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 const PageLayout = () => {
   return (
