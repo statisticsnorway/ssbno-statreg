4456b15d401cb4c3b82b96faa9fe53876e821f46
johnnadeluy <johnnadel.salita@gmail.com>
2026-06-12 12:07:40 +0200

    fix: Hide create statistic redirect button for non-admin users

--- frontend/src/views/ListStatistics.tsx
+++ frontend/src/views/ListStatistics.tsx
@@ -6,12 +6,15 @@ import { PaginatedStatisticsTable } from '../components/StatisticsTable'
 import { Heading, Button } from '@digdir/designsystemet-react'
 import { Link as ReactRouterLink } from 'react-router'
 import { PlusCircleIcon } from '@navikt/aksel-icons'
+import { useAuth } from '../context/AuthContext'
 
 export default function ListStatistics() {
   const [count, setCount] = useState(20)
   const [start, setStart] = useState(0)
   const [total, setTotal] = useState(0)
   const [statistics, setStatistics] = useState<StatisticListing[]>([])
+  const { auth } = useAuth()
+
   useEffect(() => {
     fetchStatistics(start, count)
   }, [start, count])
@@ -44,11 +47,13 @@ export default function ListStatistics() {
         <Heading level={1} data-size='sm'>
           Statistikkoversikt
         </Heading>
-        <Button datas-size='md' asChild>
-          <ReactRouterLink to='/statistikk/opprett' reloadDocument>
-            Opprett ny statistikk <PlusCircleIcon />
-          </ReactRouterLink>
-        </Button>
+        {auth?.isAdmin && (
+          <Button datas-size='md' asChild>
+            <ReactRouterLink to='/statistikk/opprett' reloadDocument>
+              Opprett ny statistikk <PlusCircleIcon />
+            </ReactRouterLink>
+          </Button>
+        )}
       </div>
       <div>
         <PaginatedStatisticsTable
