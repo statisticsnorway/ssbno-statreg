96db4768b22bddb6a5eecc13c0b376e0a91c3744
johnnadeluy <johnnadel.salita@gmail.com>
2026-06-15 09:12:00 +0200

    fix: Use ErrorPage for admin pages

--- frontend/src/views/MyPage.tsx
+++ frontend/src/views/MyPage.tsx
@@ -1,8 +1,10 @@
 import { useAuth } from '../context/AuthContext'
+import ErrorPage, { ErrorType } from './ErrorPage'
 
 export default function MyPage() {
   const { auth } = useAuth()
 
-  if (!auth?.isAdmin) return <h1>Du har ikke tilgang til denne siden.</h1>
+  if (!auth?.isAdmin) return <ErrorPage type={ErrorType.NOTAUTH} />
+
   return <h1>MyPage will be served here!</h1>
 }
