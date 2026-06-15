8b8fbea24783e9e8c84d9fda3c40ab24a12f337b
Glenruben <gle@ssb.no>
2026-06-15 09:07:07 +0200

    Mim 2784 show error page (#346)
    * Add error page, example usage
    
    * default error message fixed
    
    * why oh why can't we just get enums in js
    
    * Fix syntax on errorPage types, improve error page look somewhat
    
    * Remove duplicate function call
    
    * So we don't want to block showing the page if we aren't admin.
    
    * Add error page to createStatistic instead, even if it is just a placeholder.
    
    * Fixes after code review

--- frontend/src/views/CreateStatistic.tsx
+++ frontend/src/views/CreateStatistic.tsx
@@ -1,3 +1,10 @@
+import { useAuth } from '../context/AuthContext'
+import ErrorPage, { ErrorType } from './ErrorPage'
+
 export default function CreateStatistic() {
+  const { auth } = useAuth()
+
+  if (!auth?.isAdmin) return <ErrorPage type={ErrorType.NOTAUTH} />
+
   return <h1>CreateStatistic will be served here!</h1>
 }
