87bb1aac426198912f721331e7bc88c35ede8fcc
Olav Landsverk <stud-oll@ssb.no>
2026-06-16 16:18:44 +0200

    remove skipAuth

--- backend/src/api/controllers/contactsController.ts
+++ backend/src/api/controllers/contactsController.ts
@@ -1,11 +1,10 @@
 import { handleErrors } from '@/lib/prismaErrors'
 import { getContacts } from '@/services/contactsService'
 import { Router } from 'express'
-import { skipAuth } from 'plugins/authMiddleware'
 import { prisma } from '@/lib/prisma'
 
 export default function contactsController(router: Router) {
-  router.get('/contacts', skipAuth, async (_req, res) => {
+  router.get('/contacts', async (_req, res) => {
     try {
       const result = await getContacts(prisma)
       res.json(result)
