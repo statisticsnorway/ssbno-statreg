0d4a5c29bdd90cd4dce72bf92d6afd3b43b60947
Olav Landsverk <stud-oll@ssb.no>
2026-06-17 14:00:59 +0200

    refactor

--- frontend/src/views/ListStatistics.tsx
+++ frontend/src/views/ListStatistics.tsx
@@ -1,7 +1,7 @@
 import './ListStatistics.css'
 import { useEffect, useState } from 'react'
 import client from '../api'
-import type { ShortnameListing, StatisticListing } from '@ssbno-statreg/shared'
+import type { Contact, ShortnameListing, StatisticListing } from '@ssbno-statreg/shared'
 import { PaginatedStatisticsTable } from '../components/StatisticsTable'
 import {
   EXPERIMENTAL_Suggestion as Suggestion,
@@ -19,22 +19,32 @@ import { RowCountSelect } from '../components/RowCountSelect'
 export default function ListStatistics() {
   const [searchParams, setSearchParams] = useSearchParams()
   const shortnameQuery = searchParams.get('shortname')
+  const contactQuery = searchParams.get('contact')
   const [count, setCount] = useState(20)
   const [start, setStart] = useState(0)
   const [total, setTotal] = useState(0)
   const [statistics, setStatistics] = useState<StatisticListing[]>([])
   const [shortnames, setShortnames] = useState<ShortnameListing[]>([])
-  const [selectedShortname, setSelectedShortname] = useState<SuggestionItem | null>(null)
+  const [contacts, setContacts] = useState<Contact[]>([])
+  const [selectedFilter, setSelectedFilter] = useState<SuggestionItem | null>(null)
   const { auth } = useAuth()
 
   useEffect(() => {
-    fetchStatistics(start, count, selectedShortname)
-  }, [start, count, selectedShortname])
+    fetchStatistics(start, count, shortnameQuery, contactQuery)
+  }, [start, count, shortnameQuery, contactQuery])
 
-  const fetchStatistics = async (start: number, count: number, selectedShortname: SuggestionItem | null) => {
+  const fetchStatistics = async (
+    start: number,
+    count: number,
+    shortnameQuery: string | null,
+    contactQuery: string | null
+  ) => {
     const filter = {
-      ...(selectedShortname && {
-        shortname: selectedShortname.value,
+      ...(shortnameQuery && {
+        shortname: shortnameQuery,
+      }),
+      ...(contactQuery && {
+        contact: contactQuery,
       }),
     }
     const { data, error } = await client.GET('/statistics', {
@@ -67,16 +77,38 @@ export default function ListStatistics() {
   }, [])
 
   useEffect(() => {
-    async function setSelectedShortnameFromQuery() {
-      if (!shortnameQuery) return
-
-      setSelectedShortname({
-        label: shortnameQuery,
-        value: shortnameQuery,
-      })
+    async function fetchContacts() {
+      const { data, error } = await client.GET('/contacts')
+      if (error) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const errorMessage = (error as any).error
+        console.log(errorMessage)
+        alert(errorMessage)
+      } else {
+        setContacts(data ?? [])
+      }
     }
-    setSelectedShortnameFromQuery()
-  }, [shortnameQuery])
+    fetchContacts()
+  }, [])
+
+  useEffect(() => {
+    async function setSelectedFilterFromQuery() {
+      if (shortnameQuery) {
+        setSelectedFilter({
+          label: shortnameQuery,
+          value: `shortname:${shortnameQuery}`,
+        })
+      } else if (contactQuery) {
+        setSelectedFilter({
+          label: contactQuery,
+          value: `contact:${contactQuery}`,
+        })
+      } else {
+        setSelectedFilter(null)
+      }
+    }
+    setSelectedFilterFromQuery()
+  }, [shortnameQuery, contactQuery])
 
   function updateRowCount(newCount: number) {
     setCount(newCount)
@@ -88,15 +120,16 @@ export default function ListStatistics() {
   }
 
   function filterChanged(selected: SuggestionItem | null) {
-    setSelectedShortname(selected)
-    setSearchParams(
-      () => {
-        const next = new URLSearchParams()
-        if (selected) next.set('shortname', selected.value)
-        return next
-      },
-      { replace: true }
-    )
+    if (!selected) {
+      setSearchParams(new URLSearchParams())
+      return
+    }
+    const [category, value] = selected.value.split(':')
+    if (category === 'shortname') {
+      setSearchParams(new URLSearchParams({ shortname: value }))
+    } else if (category === 'contact') {
+      setSearchParams(new URLSearchParams({ contact: value }))
+    }
   }
 
   return (
@@ -116,17 +149,29 @@ export default function ListStatistics() {
       <div className='list-statistics-filter-container'>
         <Field>
           <Label>Filtrer statistikk</Label>
-          <Suggestion onSelectedChange={(selected) => filterChanged(selected)} selected={selectedShortname}>
+          <Suggestion onSelectedChange={(selected) => filterChanged(selected)} selected={selectedFilter}>
             <Suggestion.Input />
             <Suggestion.Clear onClick={() => filterChanged(null)} />
             <Suggestion.List>
               <Suggestion.Empty>Ingen treff</Suggestion.Empty>
-              {shortnames.map((shortname) => (
-                <Suggestion.Option key={shortname.shortname} label={shortname.shortname} value={shortname.shortname}>
-                  {shortname.shortname}
-                  <div>Kortnavn</div>
-                </Suggestion.Option>
-              ))}
+              {shortnames.map((shortname) => {
+                const value = `shortname:${shortname.shortname}`
+                return (
+                  <Suggestion.Option key={value} label={shortname.shortname} value={value}>
+                    {shortname.shortname}
+                    <div>Kortnavn</div>
+                  </Suggestion.Option>
+                )
+              })}
+              {contacts.map((contact) => {
+                const value = `contact:${contact.username}`
+                return (
+                  <Suggestion.Option key={value} label={contact.username} value={value}>
+                    {contact.username}
+                    <div>Kontakt</div>
+                  </Suggestion.Option>
+                )
+              })}
             </Suggestion.List>
           </Suggestion>
         </Field>
