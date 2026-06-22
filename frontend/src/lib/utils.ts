62220bab991b53584dca15ea65253c868f5679aa
Anne-Siri Trønnes <ntr@ssb.no>
2026-06-22 14:11:57 +0200

    show full name in list

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -65,8 +65,7 @@ export function formatVariant(variant?: Variant): string {
 export function formatContacts(contacts?: Contact[]): string[] {
   if (!contacts) return []
   return contacts.map((contact) => {
-    const initials = contact.username ?? ''
-    return initials
+    return `${contact.name!} (${contact.username!})`
   })
 }
 
