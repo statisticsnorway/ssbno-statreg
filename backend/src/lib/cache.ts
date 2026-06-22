6dc8f796594a610e6cdc161f81b593283e8b5bc0
Glenruben Larsen <gle@ssb.no>
2026-06-22 12:45:59 +0200

    Comment

--- backend/src/lib/cache.ts
+++ backend/src/lib/cache.ts
@@ -1,6 +1,8 @@
 import { NodeCache } from '@cacheable/node-cache'
 
-const timeCache = new NodeCache({ stdTTL: 20, checkperiod: 5 })
+// This is an example of how we can implement an in-memory cache of an object of any type.
+// The timeCache example should be removed before production deployment, but can serve as a template i.e. when fetching users from Entra.
+const timeCache = new NodeCache({ stdTTL: 600, checkperiod: 60 })
 
 function resetTimeCache(): Date {
   const currentTime = new Date()
