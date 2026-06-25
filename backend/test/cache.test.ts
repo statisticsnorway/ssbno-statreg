4d82a1c217b676eaf991013d0c894b93cbb82e7c
johnnadeluy <johnnadel.salita@gmail.com>
2026-06-25 13:56:23 +0200

    refactor: Refactor cache test

--- backend/test/cache.test.ts
+++ backend/test/cache.test.ts
@@ -58,54 +58,62 @@ describe('Users cache', () => {
   })
 
   test('return fetched users on first cache miss and reuse cached users', async () => {
-    await expect(getUsersFromCache()).resolves.toEqual(mockUsers)
+    const cachedUsers = await getUsersFromCache()
+    expect(cachedUsers).toEqual(mockUsers)
 
     expect(getAccessTokenMock).toHaveBeenCalledTimes(1)
     expect(fetchAllUsers).toHaveBeenCalledTimes(1)
 
-    await expect(getUsersFromCache()).resolves.toEqual(mockUsers)
+    const recachedUsers = await getUsersFromCache()
 
     expect(getAccessTokenMock).toHaveBeenCalledTimes(1)
     expect(fetchAllUsers).toHaveBeenCalledTimes(1)
+    expect(recachedUsers).toEqual(mockUsers)
   })
 
   test('store and return users', async () => {
-    await expect(setUsersCache()).resolves.toBeUndefined()
+    const setUsers = await setUsersCache()
+    expect(setUsers).toBeUndefined()
 
-    await expect(getUsersFromCache()).resolves.toEqual(mockUsers)
+    const getUsers = await getUsersFromCache()
 
     expect(getAccessTokenMock).toHaveBeenCalledTimes(1)
     expect(fetchAllUsers).toHaveBeenCalledTimes(1)
+    expect(getUsers).toEqual(mockUsers)
   })
 
   test('clear cached users', async () => {
     await setUsersCache()
-    await expect(getUsersFromCache()).resolves.toEqual(mockUsers)
+    const cachedUsers = await getUsersFromCache()
+    expect(cachedUsers).toEqual(mockUsers)
 
     clearUsersCache()
 
-    await expect(getUsersFromCache()).resolves.toEqual(mockUsers)
+    const reCachedUsers = await getUsersFromCache()
 
     expect(getAccessTokenMock).toHaveBeenCalledTimes(2)
     expect(fetchAllUsers).toHaveBeenCalledTimes(2)
+    expect(reCachedUsers).toEqual(mockUsers)
   })
 
   test('return empty array when no access token is available', async () => {
     // @ts-expect-error: Mocking the getAccessToken function to return undefined for testing purposes
     getAccessTokenMock.mockResolvedValueOnce(undefined)
 
-    await expect(getUsersFromCache()).resolves.toEqual([])
+    const users = await getUsersFromCache()
 
     expect(getAccessTokenMock).toHaveBeenCalledTimes(1)
     expect(fetchAllUsers).toHaveBeenCalledTimes(0)
+    expect(users).toEqual([])
   })
 
   test('use mock users when MOCK_ENTRA_USERS is true', async () => {
     process.env.MOCK_ENTRA_USERS = 'true'
 
-    await expect(getUsersFromCache()).resolves.toEqual(mockedEntraUsers)
+    const users = await getUsersFromCache()
 
     expect(getAccessTokenMock).toHaveBeenCalledTimes(0)
     expect(fetchAllUsers).toHaveBeenCalledTimes(0)
+    expect(users).toEqual(mockedEntraUsers)
   })
 })
