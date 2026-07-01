f70673638ea18a4af06722025e485b97a943296b
Anne-Siri Trønnes <ntr@ssb.no>
2026-07-01 20:35:02 +0200

    fix: using implemented method for parsing title

--- backend/src/services/releasesService.ts
+++ backend/src/services/releasesService.ts
@@ -13,6 +13,7 @@ import {
   parseDateOnly,
   parseId,
   ensureRequiredFieldsExists,
+  parseHumanReadableMeasuringPeriod,
 } from '@/lib/utils'
 import { ExtendedPrismaClient as PrismaClient } from '@/lib/prisma'
 import type { Prisma } from '@/generated/prisma/client'
@@ -85,7 +86,11 @@ export async function getReleases(
         approval_status: release.desk_appoval_status,
         period_to: getDateOnlyAsString(release.period_to),
         period_from: getDateOnlyAsString(release.period_from),
-        measuring_period_title: '',
+        measuring_period_title: parseHumanReadableMeasuringPeriod(
+          frequency.code,
+          release.period_from,
+          release.period_to
+        ),
         statistic: {
           shortname: statistic.shortname.name,
           name: statistic.name,
