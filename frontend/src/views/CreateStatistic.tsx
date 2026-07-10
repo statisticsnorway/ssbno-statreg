4d57f345bd15971041d5c8b5f5bedac18b511f8a
johnnadeluy <johnnadel.salita@gmail.com>
2026-07-10 10:48:48 +0200

    feat: Add success alert for shortname creation

--- frontend/src/views/CreateStatistic.tsx
+++ frontend/src/views/CreateStatistic.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from '../context/AuthContext'
 import { useState } from 'react'
+import { Alert, Heading, Paragraph } from '@digdir/designsystemet-react'
+
 import ErrorPage, { ErrorType } from './ErrorPage'
 import { CreateShortnameModal } from '../components/CreateShortnameModal'
 
@@ -11,6 +13,15 @@ export default function CreateStatistic() {
 
   return (
     <>
+      <Alert data-color='success'>
+        <Heading level={2} data-size='xs'>
+          Kortnavnet er nå registrert i systemet
+        </Heading>
+        <Paragraph>
+          Fyll ut resten av informasjonen. Alle obligatoriske felter må fylles ut før du kan opprette den endelige
+          statistikken.
+        </Paragraph>
+      </Alert>
       <CreateShortnameModal
         openCreateShortnameModal={openCreateShortnameModal}
         setOpenCreateReleaseModal={setOpenCreateReleaseModal}
