19fc47dca5a2289caf8c1c0592867e7efe5ac74f
johnnadeluy <johnnadel.salita@gmail.com>
2026-07-14 13:54:24 +0200

    feat: Add form for statistic creation [MIM-2675]

--- frontend/src/views/CreateStatistic.tsx
+++ frontend/src/views/CreateStatistic.tsx
@@ -1,6 +1,22 @@
 import { useAuth } from '../context/AuthContext'
 import { useState } from 'react'
-import { Alert, Heading, Paragraph } from '@digdir/designsystemet-react'
+import {
+  Alert,
+  Heading,
+  Popover,
+  Paragraph,
+  Divider,
+  Field,
+  Fieldset,
+  Checkbox,
+  Label,
+  Select,
+  Input,
+  Button,
+} from '@digdir/designsystemet-react'
+import { QuestionmarkCircleIcon } from '@navikt/aksel-icons'
+
+import './CreateStatistic.css'
 
 import type { Shortname } from '@ssbno-statreg/shared'
 import ErrorPage, { ErrorType } from './ErrorPage'
@@ -34,6 +50,91 @@ export default function CreateStatistic() {
           setCreatedShortname={setCreatedShortname}
         />
       )}
+      <Heading level={1} data-size='md'>
+        Opprett statistikk
+      </Heading>
+      <form className='create-statistic-form'>
+        <Field>
+          <div className='create-statistic-form-status-label'>
+            <Label data-size='lg'>Status</Label>
+            <Popover.TriggerContext>
+              <Popover.Trigger variant='tertiary'>
+                <QuestionmarkCircleIcon fontSize={24} />
+              </Popover.Trigger>
+              <Popover placement='right' data-color='info'>
+                <ul>
+                  <li>
+                    Statistikker som har blitt opprettet med status «Aktiv», kan ikke bli gjort om til «Kommende» igjen.
+                  </li>
+                  <li>
+                    For å slette en statistikk som har blitt feilopprettet må du ta kontakt med mailadresse@ssb.no
+                  </li>
+                </ul>
+              </Popover>
+            </Popover.TriggerContext>
+          </div>
+          <Field.Description>
+            Statistikker som er nyopprettet får status «Kommende». For å sette den til «Aktiv» må du i tillegg fylle ut:
+            Engelsk navn, varianter og målform.
+          </Field.Description>
+          <Select defaultValue='upcoming'>
+            <Select.Option value='upcoming'>Kommende</Select.Option>
+          </Select>
+        </Field>
+        <Divider />
+        <Heading level={2}>Navn</Heading>
+        <Field>
+          <Label>Kortnavn</Label>
+          <Field.Description>Kortnavnet kan ikke endres etter statistikken har blitt opprettet.</Field.Description>
+          <Input readOnly value={createdShortname?.shortname} />
+        </Field>
+        <Field>
+          <Label>Norsk statistikknavn</Label>
+          <Input />
+        </Field>
+        <Field>
+          <Label>Engelsk statistikknavn</Label>
+          <Input />
+        </Field>
+        <Divider />
+        <Heading level={2}>Detaljer</Heading>
+        <Field>
+          <Label>Seksjon</Label>
+          <Select defaultValue=''>
+            <Select.Option value='123'>Seksjon for ...</Select.Option>
+          </Select>
+        </Field>
+        <Fieldset>
+          <Fieldset.Legend>Regionale nivåer</Fieldset.Legend>
+          <Checkbox label='Bydel og krets' />
+          <Checkbox label='Kommune' />
+          <Checkbox label='Fylke' />
+          <Checkbox label='Landsdel' />
+          <Checkbox label='Land' />
+        </Fieldset>
+        <Field>
+          <Label>Målform</Label>
+          <Select defaultValue=''>
+            <Select.Option value='nb'>Bokmål</Select.Option>
+            <Select.Option value='nn'>Nynorsk</Select.Option>
+          </Select>
+        </Field>
+        <Field>
+          <Label>Statistikkens startår</Label>
+          <Field.Description>F.eks 1876</Field.Description>
+          <Input />
+        </Field>
+        <Divider />
+        <Field>
+          <Label>Kommentar (Valgfritt)</Label>
+          <Field.Description>Annen relevant informasjon.</Field.Description>
+          <Input />
+        </Field>
+        <div className='create-statistic-form-buttons'>
+          <Button type='submit'>Opprett</Button>
+          <Button variant='tertiary'>Avbryt</Button>
+        </div>
+      </form>
     </>
   )
 }
