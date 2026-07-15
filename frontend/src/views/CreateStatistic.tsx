489fcd717d65557976b1b74f7f646d4a3597182f
johnnadeluy <johnnadel.salita@gmail.com>
2026-07-15 09:04:23 +0200

    fix: Render form after modal is closed

--- frontend/src/views/CreateStatistic.tsx
+++ frontend/src/views/CreateStatistic.tsx
@@ -85,124 +85,126 @@ export default function CreateStatistic() {
         />
       )}
 
-      <div className='create-statistic-container'>
-        {createdShortname && (
-          <Alert data-color='success'>
-            <Heading level={2} data-size='xs'>
-              Kortnavnet er nå registrert i systemet
-            </Heading>
-            <Paragraph>
-              Fyll ut resten av informasjonen. Alle obligatoriske felter må fylles ut før du kan opprette den endelige
-              statistikken.
-            </Paragraph>
-          </Alert>
-        )}
+      {!openCreateShortnameModal && (
+        <div className='create-statistic-container'>
+          {createdShortname && (
+            <Alert data-color='success'>
+              <Heading level={2} data-size='xs'>
+                Kortnavnet er nå registrert i systemet
+              </Heading>
+              <Paragraph>
+                Fyll ut resten av informasjonen. Alle obligatoriske felter må fylles ut før du kan opprette den endelige
+                statistikken.
+              </Paragraph>
+            </Alert>
+          )}
 
-        <Heading level={1} data-size='md' className='create-statistic-heading'>
-          Opprett statistikk
-        </Heading>
+          <Heading level={1} data-size='md' className='create-statistic-heading'>
+            Opprett statistikk
+          </Heading>
 
-        <form className='create-statistic-form' onSubmit={handleSubmit}>
-          <Field>
-            <div className='create-statistic-form-status-label'>
-              <Label data-size='lg'>Status</Label>
-              <Popover.TriggerContext>
-                <Popover.Trigger variant='tertiary'>
-                  <QuestionmarkCircleIcon fontSize={24} />
-                </Popover.Trigger>
-                <Popover placement='right' data-color='info'>
-                  <ul>
-                    <li>
-                      Statistikker som har blitt opprettet med status «Aktiv», kan ikke bli gjort om til «Kommende»
-                      igjen.
-                    </li>
-                    <li>
-                      For å slette en statistikk som har blitt feilopprettet må du ta kontakt med mailadresse@ssb.no
-                    </li>
-                  </ul>
-                </Popover>
-              </Popover.TriggerContext>
-            </div>
-            <Field.Description>
-              Statistikker som er nyopprettet får status «Kommende». For å sette den til «Aktiv» må du i tillegg fylle
-              ut: Engelsk navn, varianter og målform.
-            </Field.Description>
-            <Select defaultValue='K' onChange={(e) => setValues({ ...values, status: e.target.value })}>
-              <Select.Option value='K'>Kommende</Select.Option>
-            </Select>
-          </Field>
-          <Divider />
-          <Heading level={2}>Navn</Heading>
-          <Field>
-            <Label>Kortnavn</Label>
-            <Field.Description>Kortnavnet kan ikke endres etter statistikken har blitt opprettet.</Field.Description>
-            <Input readOnly value={createdShortname?.shortname} />
-          </Field>
-          <Field>
-            <Label>Norsk statistikknavn</Label>
-            <Input
-              value={values.name}
-              onChange={(e) => {
-                setValues((values) => ({ ...values, name: e.target.value }))
-              }}
-            />
-          </Field>
-          <Field>
-            <Label>Engelsk statistikknavn</Label>
-            <Input
-              value={values.name_en}
-              onChange={(e) => {
-                setValues((values) => ({ ...values, name_en: e.target.value }))
-              }}
-            />
-          </Field>
-          <Divider />
-          <Heading level={2}>Detaljer</Heading>
-          <Field>
-            <Label>Seksjon</Label>
-            <Select defaultValue='' onChange={(e) => setValues({ ...values, division: e.target.value })}>
-              <Select.Option value='' disabled />
-              <Select.Option value='123'>Seksjon for ...</Select.Option>
-            </Select>
-          </Field>
-          <Fieldset>
-            <Fieldset.Legend>Regionale nivåer</Fieldset.Legend>
-            {regionLevelCheckboxes.map((regionLevel) => (
-              <Checkbox
-                key={`region-level-checkbox-${regionLevel.code}`}
-                label={regionLevel.name}
-                {...getCheckboxProps(regionLevel.code)}
+          <form className='create-statistic-form' onSubmit={handleSubmit}>
+            <Field>
+              <div className='create-statistic-form-status-label'>
+                <Label data-size='lg'>Status</Label>
+                <Popover.TriggerContext>
+                  <Popover.Trigger variant='tertiary'>
+                    <QuestionmarkCircleIcon fontSize={24} />
+                  </Popover.Trigger>
+                  <Popover placement='right' data-color='info'>
+                    <ul>
+                      <li>
+                        Statistikker som har blitt opprettet med status «Aktiv», kan ikke bli gjort om til «Kommende»
+                        igjen.
+                      </li>
+                      <li>
+                        For å slette en statistikk som har blitt feilopprettet må du ta kontakt med mailadresse@ssb.no
+                      </li>
+                    </ul>
+                  </Popover>
+                </Popover.TriggerContext>
+              </div>
+              <Field.Description>
+                Statistikker som er nyopprettet får status «Kommende». For å sette den til «Aktiv» må du i tillegg fylle
+                ut: Engelsk navn, varianter og målform.
+              </Field.Description>
+              <Select defaultValue='K' onChange={(e) => setValues({ ...values, status: e.target.value })}>
+                <Select.Option value='K'>Kommende</Select.Option>
+              </Select>
+            </Field>
+            <Divider />
+            <Heading level={2}>Navn</Heading>
+            <Field>
+              <Label>Kortnavn</Label>
+              <Field.Description>Kortnavnet kan ikke endres etter statistikken har blitt opprettet.</Field.Description>
+              <Input readOnly value={createdShortname?.shortname} />
+            </Field>
+            <Field>
+              <Label>Norsk statistikknavn</Label>
+              <Input
+                value={values.name}
+                onChange={(e) => {
+                  setValues((values) => ({ ...values, name: e.target.value }))
+                }}
               />
-            ))}
-          </Fieldset>
-          <Field>
-            <Label>Målform</Label>
-            <Select defaultValue='' onChange={(e) => setValues({ ...values, main_language: e.target.value })}>
-              <Select.Option value='' disabled />
-              <Select.Option value='nb'>Bokmål</Select.Option>
-              <Select.Option value='nn'>Nynorsk</Select.Option>
-            </Select>
-          </Field>
-          <Field>
-            <Label>Statistikkens startår</Label>
-            <Field.Description>F.eks 1876</Field.Description>
-            <Input
-              value={values.first_released_at}
-              onChange={(e) => setValues({ ...values, first_released_at: e.target.value })}
-            />
-          </Field>
-          <Divider />
-          <Field>
-            <Label>Kommentar (Valgfritt)</Label>
-            <Field.Description>Annen relevant informasjon.</Field.Description>
-            <Input value={values.comment} onChange={(e) => setValues({ ...values, comment: e.target.value })} />
-          </Field>
-          <div className='create-statistic-form-buttons'>
-            <Button type='submit'>Opprett</Button>
-            <Button variant='tertiary'>Avbryt</Button>
-          </div>
-        </form>
-      </div>
+            </Field>
+            <Field>
+              <Label>Engelsk statistikknavn</Label>
+              <Input
+                value={values.name_en}
+                onChange={(e) => {
+                  setValues((values) => ({ ...values, name_en: e.target.value }))
+                }}
+              />
+            </Field>
+            <Divider />
+            <Heading level={2}>Detaljer</Heading>
+            <Field>
+              <Label>Seksjon</Label>
+              <Select defaultValue='' onChange={(e) => setValues({ ...values, division: e.target.value })}>
+                <Select.Option value='' disabled />
+                <Select.Option value='123'>Seksjon for ...</Select.Option>
+              </Select>
+            </Field>
+            <Fieldset>
+              <Fieldset.Legend>Regionale nivåer</Fieldset.Legend>
+              {regionLevelCheckboxes.map((regionLevel) => (
+                <Checkbox
+                  key={`region-level-checkbox-${regionLevel.code}`}
+                  label={regionLevel.name}
+                  {...getCheckboxProps(regionLevel.code)}
+                />
+              ))}
+            </Fieldset>
+            <Field>
+              <Label>Målform</Label>
+              <Select defaultValue='' onChange={(e) => setValues({ ...values, main_language: e.target.value })}>
+                <Select.Option value='' disabled />
+                <Select.Option value='nb'>Bokmål</Select.Option>
+                <Select.Option value='nn'>Nynorsk</Select.Option>
+              </Select>
+            </Field>
+            <Field>
+              <Label>Statistikkens startår</Label>
+              <Field.Description>F.eks 1876</Field.Description>
+              <Input
+                value={values.first_released_at}
+                onChange={(e) => setValues({ ...values, first_released_at: e.target.value })}
+              />
+            </Field>
+            <Divider />
+            <Field>
+              <Label>Kommentar (Valgfritt)</Label>
+              <Field.Description>Annen relevant informasjon.</Field.Description>
+              <Input value={values.comment} onChange={(e) => setValues({ ...values, comment: e.target.value })} />
+            </Field>
+            <div className='create-statistic-form-buttons'>
+              <Button type='submit'>Opprett</Button>
+              <Button variant='tertiary'>Avbryt</Button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   )
 }
