b0d2ba0982104f1f2474b3dd76e66c159830447b
Olav Landsverk <stud-oll@ssb.no>
2026-08-05 15:55:04 +0200

    first attempt

--- shared/src/api-types.ts
+++ shared/src/api-types.ts
@@ -464,23 +464,25 @@ export interface paths {
       path?: never
       cookie?: never
     }
-    /** List all versions with changes and comment. Response object TBD */
+    /** List all versions with changes and comment for a statistic */
     get: {
       parameters: {
         query?: never
         header?: never
-        path?: never
+        path: {
+          shortname: string
+        }
         cookie?: never
       }
       requestBody?: never
       responses: {
-        /** @description List of versions. Response object TBD */
+        /** @description List of versions for the statistic */
         200: {
           headers: {
             [name: string]: unknown
           }
           content: {
-            'application/json': unknown
+            'application/json': components['schemas']['Version'][]
           }
         }
         default: components['responses']['Error']
@@ -763,25 +765,25 @@ export interface paths {
       path?: never
       cookie?: never
     }
-    /** List all versions on a release with change and comment. Response object TBD */
+    /** List all versions on a release with changes and comment */
     get: {
       parameters: {
         query?: never
         header?: never
         path: {
-          id: string
+          id: number
         }
         cookie?: never
       }
       requestBody?: never
       responses: {
-        /** @description List of versions. Response object TBD */
+        /** @description List of versions for the release */
         200: {
           headers: {
             [name: string]: unknown
           }
           content: {
-            'application/json': unknown
+            'application/json': components['schemas']['Version'][]
           }
         }
         default: components['responses']['Error']
@@ -1191,6 +1193,23 @@ export interface components {
       division?: components['schemas']['Division']
       contacts?: components['schemas']['Contact'][]
     } & components['schemas']['Statistic']
+    Version: {
+      /** @enum {string} */
+      change_type: 'create' | 'update' | 'delete'
+      /** Format: date-time */
+      changed_at: string
+      /** @description The principal name of the user who made the change. */
+      changed_by: string
+      changed_values: {
+        /** @description The name of the field that changed. */
+        field_name: string
+        /** @description Human-readable version of the old value. Null if change_type is "create". */
+        old_value: string | null
+        /** @description Human-readable version of the new value. Null if change_type is "delete". */
+        new_value: string | null
+      }[]
+      comment?: string
+    }
     Variant: {
       readonly id?: number
       version?: number
