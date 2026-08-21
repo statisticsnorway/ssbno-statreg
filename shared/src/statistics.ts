53a29d5b296ed0e07b3d41a1b3e608bb1ca120f4
johnnadeluy <johnnadel.salita@gmail.com>
2026-08-21 11:46:07 +0200

    refactor: Remove unecessary helper functions

--- shared/src/statistics.ts
+++ shared/src/statistics.ts
@@ -5,29 +5,21 @@ export type CreatableStatisticStatus = 'K' | 'A'
 export type EditableStatisticStatus = keyof typeof StatisticStatus
 type RequiredStatisticField = keyof StatisticCreate
 
-export const requiredStatisticFieldsByStatus: Record<CreatableStatisticStatus, RequiredStatisticField[]> = {
+export const RequiredCreateStatisticFieldsByStatus: Record<CreatableStatisticStatus, RequiredStatisticField[]> = {
   K: ['name', 'division'],
   A: ['name', 'name_en', 'variants', 'contacts', 'division'],
 }
 
-const requiredEditStatisticFieldsByStatus: Partial<Record<EditableStatisticStatus, RequiredStatisticField[]>> =
-  requiredStatisticFieldsByStatus
-
-export function getRequiredStatisticFields(status: CreatableStatisticStatus): RequiredStatisticField[] {
-  return requiredStatisticFieldsByStatus[status]
-}
-
-export function getRequiredEditStatisticFields(status: EditableStatisticStatus): RequiredStatisticField[] {
-  return requiredEditStatisticFieldsByStatus[status] ?? []
-}
+export const RequiredEditStatisticFieldsByStatus: Partial<Record<EditableStatisticStatus, RequiredStatisticField[]>> =
+  RequiredCreateStatisticFieldsByStatus
 
 export function isCreateStatisticFieldRequired(
   status: CreatableStatisticStatus,
   field: RequiredStatisticField
 ): boolean {
-  return requiredStatisticFieldsByStatus[status].includes(field)
+  return RequiredCreateStatisticFieldsByStatus[status].includes(field)
 }
 
 export function isEditStatisticFieldRequired(status: EditableStatisticStatus, field: RequiredStatisticField): boolean {
-  return getRequiredEditStatisticFields(status).includes(field)
+  return RequiredEditStatisticFieldsByStatus[status]?.includes(field) ?? false
 }
