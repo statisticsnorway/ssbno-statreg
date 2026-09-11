4bd3626766f712b4ffb282e72d917d5176864d75
Glenruben Larsen <gle@ssb.no>
2026-09-11 09:28:24 +0200

    Split API-types into separate package, set up imports and typing from there

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,5 @@
 import createClient from 'openapi-fetch'
-import type { paths } from '../../shared/src/api-types'
+import type { paths } from '@statisticsnorway/statreg-api-types'
 
 const client = createClient<paths>({ baseUrl: '/statistikkregisteret/api' })
 
